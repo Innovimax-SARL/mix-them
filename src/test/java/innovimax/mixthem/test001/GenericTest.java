f8ea99350be35b869b8208b4e8b6730271a7cdec
Innovimax <innovimax@gmail.com>
2017-02-03 19:32:04 +0100

    use ByteArrayOutputStream

--- src/test/java/innovimax/mixthem/test001/GenericTest.java
+++ src/test/java/innovimax/mixthem/test001/GenericTest.java
@@ -14,23 +14,29 @@ import org.junit.Test;
 */
 public class GenericTest {
 
-	@Test
-  	public final void check1() {
-      ClassLoader classLoader = getClass().getClassLoader();
-      File file1 = new File(classLoader.getResource("file1.txt").getFile());
-      File file2 = new File(classLoader.getResource("file2.txt").getFile());
-      Assert.assertTrue(checkFileEquals(file1, MixThem.processFiles(Constants.RULE_1, file1, file2)));
-      Assert.assertTrue(checkFileEquals(file2, MixThem.processFiles(Constants.RULE_2, file1, file2)));
-  	}
-    private static boolean checkFileEquals(File fileExpected, File fileResult) {
+   @Test
+   public final void check1() {
+          ClassLoader classLoader = getClass().getClassLoader();
+          File file1 = new File(classLoader.getResource("file1.txt").getFile());
+          File file2 = new File(classLoader.getResource("file2.txt").getFile());
+          ByteArrayOutputStream baos_rule_1 = new ByteArrayOutputStream();
+          MixThem.processFiles(Constants.RULE_1, file1, file2, baos_rule_1);
+          Assert.assertTrue(checkFileEquals(file1, baos_rule_1.toByteArray()));
+          ByteArrayOutputStream baos_rule_2 = new ByteArrayOutputStream();
+          MixThem.processFiles(Constants.RULE_2, file1, file2, baos_rule_2);
+          Assert.assertTrue(checkFileEquals(file2, baos_rule_2.toByteArray()));
+    }
+    private static boolean checkFileEquals(File fileExpected, byte[] result) {
        FileInputStream fisExpected = new FileInputStream(fileExpected);
-       FileInputStream fisResult = new FileInputStream(fileResult);
        int c;
+       int offset = 0;
        while ((c = fisExpected.read()) != -1) {
-          int d = fisResult.read();
+	   if (offset >= result.length) return false;
+          int d = byte[offset++];
           if (c != d) return false;
        }
-       int e = fisResult.read();
+	   if (offset >= result.length) return false;
+          int e = byte[offset++];
        if (c != e) return false;
        return true;
     }
