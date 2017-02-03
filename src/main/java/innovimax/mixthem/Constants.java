67b5b18780cc6ca3c55c08e29a2e44096d6019ce
Innovimax <innovimax@gmail.com>
2017-02-03 18:40:50 +0100

    Create Constants.java

---
+++ src/main/java/innovimax/mixthem/Constants.java
@@ -0,0 +1,9 @@
+package innovimax.mixthem;
+
+public class Constants {
+   public final static String RULE_1 = "1";
+   public final static String RULE_2 = "2";
+   public final static String RULE_ALT_LINE = "alt-line";
+   public final static String RULE_ALT_BYTE = "alt-byte";
+   public final static String RULE_RANDOM_ALT_LINE = "random-alt-line";
+}
