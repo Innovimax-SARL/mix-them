8139d1ee4355a4355f49cf39bd22bd372c5fc0f7
Innovimax <innovimax@gmail.com>
2017-02-06 15:23:01 +0100

    replace ALT_BYTE by ALT_CHAR

--- src/main/java/innovimax/mixthem/Constants.java
+++ src/main/java/innovimax/mixthem/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
    public final static String RULE_1 = "1";
    public final static String RULE_2 = "2";
    public final static String RULE_ALT_LINE = "alt-line";
-   public final static String RULE_ALT_BYTE = "alt-byte";
+   public final static String RULE_ALT_CHAR = "alt-byte";
    public final static String RULE_RANDOM_ALT_LINE = "random-alt-line";
    public final static String RULE_JOIN = "join";
 }
