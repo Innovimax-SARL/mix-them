4c290ebe8e30c93427b8ab0b3dbb5757e0c69add
Innovimax team <kojimch@gmail.com>
2017-02-07 16:05:17 +0100

    Create DefaultCharWriter.java

---
+++ src/main/java/innovimax/mixthem/io/DefaultCharWriter.java
@@ -0,0 +1,33 @@
+package innovimax.mixthem.io;
+
+import innovimax.mixthem.interfaces.IOutputChar;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+/*
+    Created by innovimax
+    Default implementation of IOutputChar
+*/
+public class DefaultCharWriter implements IOutputChar {
+
+	private Writer writer = null;
+
+	public DefaultCharWriter(OutputStream output) throws IOException {
+		this.writer = new BufferedWriter(new OutputStreamWriter(output));
+	}
+
+	@Override
+	public void writeCharacter(int c) throws IOException {		
+		this.writer.write(c);
+	}
+
+	@Override
+	public void writeCharacters(char[] buffer, int len) throws IOException {				
+		this.writer.write(buffer, 0, len);
+	}
+
+}
