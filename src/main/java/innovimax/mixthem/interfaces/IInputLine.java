1f3ef2bfe9152b3a575119f3ca53a4901a48ecc0
Innovimax team <kojimch@gmail.com>
2017-02-16 14:40:18 +0100

    Update IInputLine.java
    remove not streaming methods

--- src/main/java/innovimax/mixthem/interfaces/IInputLine.java
+++ src/main/java/innovimax/mixthem/interfaces/IInputLine.java
@@ -13,37 +13,16 @@ public interface IInputLine {
 	* @return Returns true if there is more lines
 	* @throws IOException - If an I/O error occurs
 	*/	
-	boolean hasLine() throws IOException;
+	public boolean hasLine() throws IOException;
 	/**
  	* Reads a line of characters
  	* @return The line of characters read, or null if there is no more lines.
  	* @throws IOException - If an I/O error occurs
  	*/	
-	String nextLine() throws IOException;
+	public String nextLine() throws IOException;
 	/**
 	* Closes this input and releases any system resources associated with it.
 	* @throws IOException - If an I/O error occurs
 	*/
-  void close() throws IOException;	
-
-	/**
-	* Preloads this input by reading all the lines of characters in memory.
-	* @throws IOException - If an I/O error occurs
-	*/
-	void preload() throws IOException;		
-	/**
-	* Returns the number of lines preloaded.
-	* @return The number of lines preloaded, or -1 if no preloading nas been done
-	*/	
-	int size();
-	/**
-	* Returns true if there is a line with the given index.
-	* @return Returns true if this line exists
-	*/	
-	boolean hasLine(int index);
-	/**
- 	* Reads the line with the given index
- 	* @return The line with the given index, or null if there is not exists.
- 	*/	
-	String getLine(int index);
+	public void close() throws IOException;
 }
