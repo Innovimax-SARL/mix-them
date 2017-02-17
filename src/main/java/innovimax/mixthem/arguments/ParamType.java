f71294b468555ab1284f92a059b2d4dd2ae138c5
Innovimax team <kojimch@gmail.com>
2017-02-17 09:57:13 +0100

    Relocate ParamType.java

---
+++ src/main/java/innovimax/mixthem/arguments/ParamType.java
@@ -0,0 +1,11 @@
+package innovimax.mixthem.arguments;
+
+/**
+* <p>This is a enumeration of rule parameters types.</p>
+* @author Innovimax
+* @version 1.0
+*/
+public enum ParamType { 
+	_STRING,
+	_INTEGER;
+}
