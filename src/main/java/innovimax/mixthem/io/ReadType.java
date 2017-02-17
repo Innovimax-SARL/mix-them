e516198c338c9f6bd811edcc0863d083b944225d
Innovimax team <kojimch@gmail.com>
2017-02-17 12:41:22 +0100

    Relocate ReadType.java

---
+++ src/main/java/innovimax/mixthem/io/ReadType.java
@@ -0,0 +1,18 @@
+package innovimax.mixthem.io;
+
+/**
+* <p>This is a enumeration of reading type.</p>
+* <p>Here are the types:</p>
+* <ul>
+* <li>_REGULAR: regular way, read each line/character</li>
+* <li>_ALT_SIMPLE: simple alternation, read every two lines/characters</li>
+* <li>_ALT_RANDOM: random alternation, read randomly based on a seed</li>
+* </ul>
+* @author Innovimax
+* @version 1.0
+*/
+public enum ReadType {
+	_REGULAR,
+	_ALT_SIMPLE,
+	_ALT_RANDOM;
+}
