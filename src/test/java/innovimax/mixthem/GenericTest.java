08f07f1bd5e15dc6bfd5d08a29055849a294a5d7
Innovimax <innovimax@gmail.com>
2017-02-17 17:44:59 +0100

    merge in one test

--- src/test/java/innovimax/mixthem/GenericTest.java
+++ src/test/java/innovimax/mixthem/GenericTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 	Generic tests for this application
 */
 public class GenericTest {
+   
    @Test
    public final void parameter() throws MixException, FileNotFoundException, IOException {
 	   
@@ -31,12 +32,21 @@ public class GenericTest {
 			   String resource = prefix+"output-"+ rule.getExtension()+".txt";
 			   URL url = getClass().getResource(resource);
 			   System.out.println(rule+" implemented = "+rule.isImplemented()+" ; resource ("+resource+") = "+url);
+			   if (rule.isImplemeted() && url != null) {
+			   	check(new File(url1), new File(url2), new File(url), rule);
+			   }
 		   }   
 		   i++;
 	   }
 	   Assert.assertTrue(true);
    }	   
-	
+   private final static void check(File file1, File file2, File expected, Rule rule)  throws MixException, FileNotFoundException, IOException  {
+	   ByteArrayOutputStream baos_rule = new ByteArrayOutputStream();
+	   MixThem mixThem = new MixThem(file1, file2, baos_rule);
+           mixThem.process(rule);
+	   Assert.assertTrue(checkFileEquals(expected, baos_rule.toByteArray()));
+   }
+	/*
    @Test
    public final void checkRule1() throws MixException, FileNotFoundException, IOException {
 		URL url1 = getClass().getResource("test001_file1.txt");
@@ -140,15 +150,15 @@ public class GenericTest {
 		System.out.println("test002/Mixed/alt-char:");
 		mixThem = new MixThem(file1, file2, System.out);
 		mixThem.process(Rule._ALT_CHAR);
-		/*
-		System.out.println("test002/Expected/alt-char:");
-		String line;
-		BufferedReader br = new BufferedReader(new FileReader(fileComp));
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
-		}
-		br.close();
-		*/
+		
+		//System.out.println("test002/Expected/alt-char:");
+		//String line;
+		//BufferedReader br = new BufferedReader(new FileReader(fileComp));
+		//while ((line = br.readLine()) != null) {
+		//	System.out.println(line);
+		//}
+		//br.close();
+		
 	}
 
 	@Test
@@ -162,15 +172,15 @@ public class GenericTest {
 		System.out.println("test001/Mixed/alt-line:");
 		MixThem mixThem = new MixThem(file1, file2, System.out);
 		mixThem.process(Rule._ALT_LINE);  
-		/*        
-		System.out.println("test001/Expected/alt-line:");
-		String line;
-		BufferedReader br = new BufferedReader(new FileReader(fileComp));
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
-		}
-		br.close();
-		*/
+		        
+		//System.out.println("test001/Expected/alt-line:");
+		//String line;
+		//BufferedReader br = new BufferedReader(new FileReader(fileComp));
+		//while ((line = br.readLine()) != null) {
+		//	System.out.println(line);
+		//}
+		//br.close();
+		
 	}
 
 	@Test
@@ -184,16 +194,17 @@ public class GenericTest {
 		System.out.println("test002/Mixed/random-alt-line:");
 		MixThem mixThem = new MixThem(file1, file2, System.out);
 		mixThem.process(Rule._RANDOM_ALT_LINE);  
-		/*
-		System.out.println("test001/Expected/random-alt-line:");
-		String line;
-		BufferedReader br = new BufferedReader(new FileReader(fileComp));
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
-		}
-		br.close();
-		*/
+		
+		//System.out.println("test001/Expected/random-alt-line:");
+		//String line;
+		//BufferedReader br = new BufferedReader(new FileReader(fileComp));
+		//while ((line = br.readLine()) != null) {
+		//	System.out.println(line);
+		//}
+		//br.close();
+		
 	}
+	*/
 
 	private static boolean checkFileEquals(File fileExpected, byte[] result) throws FileNotFoundException, IOException {
 	   FileInputStream fisExpected = new FileInputStream(fileExpected);
