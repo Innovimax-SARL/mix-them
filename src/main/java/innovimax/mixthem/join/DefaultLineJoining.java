ad70ed9dcd2ccb74001b98dfd3b6806816e88543
Innovimax team <kojimch@gmail.com>
2017-02-20 15:40:39 +0100

    Update DefaultLineJoining.java
    fix pmd

--- src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
@@ -18,15 +18,13 @@ public class DefaultLineJoining implements IJoinLine {
 
 	@Override
 	public String join(String line1, String line2) throws MixException {
-		if ((line1 != null) && (line2 != null)) {
+		if (line1 != null && line2 != null) {
 			List<String> list1 = Arrays.asList(line1.split("\\s"));
 			List<String> list2 = Arrays.asList(line2.split("\\s"));
-			if (list1.size() > 0) {
-				if (list2.contains(list1.get(0))) {
-					String part1 = list1.stream().collect(Collectors.joining(" "));
-					String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
-					return part1 + " " + part2;
-				}
+			if (list1.size() > 0 && list2.contains(list1.get(0))) {
+				String part1 = list1.stream().collect(Collectors.joining(" "));
+				String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+				return part1 + " " + part2;				
 			}
 		}
 		return null;
