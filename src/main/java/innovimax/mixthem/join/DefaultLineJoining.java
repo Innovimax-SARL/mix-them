1ffa07b01a01d2ee76a1ed6e4d51df84e66834aa
Innovimax team <kojimch@gmail.com>
2017-02-20 12:09:48 +0100

    Update DefaultLineJoining.java

--- src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
@@ -3,6 +3,10 @@ package innovimax.mixthem.join;
 import innovimax.mixthem.exceptions.MixException;
 import innovimax.mixthem.interfaces.IJoinLine;
 
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
 /**
 * <p>Joins two lines on a common field.</p>
 * <p>This is the default implementation of IJoinLine.</p>
@@ -14,7 +18,22 @@ public class DefaultLineJoining implements IJoinLine {
 
 	@Override
 	public String join(String line1, String line2) throws MixException {
-		return line1 + line2;
+		LinkedList<String> list1 = new LinkedList<String>();
+		StringTokenizer st1 = new StringTokenizer(" ");
+		while (st1.hasMoreTokens()) {
+			list1.add(st1.nextToken());
+		}
+		LinkedList<String> list2 = new LinkedList<String>();
+		StringTokenizer st2 = new StringTokenizer(" ");
+		while (st2.hasMoreTokens()) {
+			list2.add(st2.nextToken());
+		}
+		if (list2.contains(list1.getFirst())) {
+			String part1 = list1.stream().collect(Collectors.joining(" "));
+			String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+			return part1 + " " + part2;
+		}
+		return null;
 	}
 
 }
