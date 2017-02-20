0c4357aba16080922011ea2fc4d71ae71103a749
Innovimax team <kojimch@gmail.com>
2017-02-20 15:35:22 +0100

    Update DefaultLineJoining.java
    simplify code

--- src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
@@ -3,8 +3,8 @@ package innovimax.mixthem.join;
 import innovimax.mixthem.exceptions.MixException;
 import innovimax.mixthem.interfaces.IJoinLine;
 
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -18,20 +18,16 @@ public class DefaultLineJoining implements IJoinLine {
 
 	@Override
 	public String join(String line1, String line2) throws MixException {
-		LinkedList<String> list1 = new LinkedList<String>();
-		StringTokenizer st1 = new StringTokenizer(line1, " ");
-		while (st1.hasMoreTokens()) {
-			list1.add(st1.nextToken());
-		}
-		LinkedList<String> list2 = new LinkedList<String>();
-		StringTokenizer st2 = new StringTokenizer(line2, " ");
-		while (st2.hasMoreTokens()) {
-			list2.add(st2.nextToken());
-		}
-		if (list2.contains(list1.getFirst())) {
-			String part1 = list1.stream().collect(Collectors.joining(" "));
-			String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
-			return part1 + " " + part2;
+		if ((line1 != null) && (line2 != null)) {
+			List<String> list1 = Arrays.asList(line1.split("\\s"));
+			List<String> list2 = Arrays.asList(line2.split("\\s"));
+			if (list1.size() > 0) {
+				if (list2.contains(list1.get(0))) {
+					String part1 = list1.stream().collect(Collectors.joining(" "));
+					String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+					return part1 + " " + part2;
+				}
+			}
 		}
 		return null;
 	}
