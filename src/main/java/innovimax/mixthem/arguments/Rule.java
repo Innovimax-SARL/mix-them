ffc42c8b42f5bab374146a17845f1a3c13b87362
Innovimax team <kojimch@gmail.com>
2017-02-21 17:31:32 +0100

    Update Rule.java
    add parameters testing values

--- src/main/java/innovimax/mixthem/arguments/Rule.java
+++ src/main/java/innovimax/mixthem/arguments/Rule.java
@@ -1,6 +1,9 @@
 package innovimax.mixthem.arguments;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
+import java.util.List;
 
 /**
 * <p>This is a detailed enumeration of rules used to mix files.</p>
@@ -10,25 +13,27 @@ import java.util.EnumSet;
 * @version 1.0
 */
 public enum Rule { 
-    _1(              "1",               "1",               "will output file1", true, EnumSet.noneOf(RuleParam.class)),
-    _2(              "2",               "2",               "will output file2", true, EnumSet.noneOf(RuleParam.class)),
-    _ADD(            "+",               "add",             "will output file1+file2", true, EnumSet.noneOf(RuleParam.class)),
-    _ALT_LINE(       "alt-line",        "altline",        "will output one line of each starting with first line of file1", true, EnumSet.noneOf(RuleParam.class)),
-    _ALT_CHAR(       "alt-char",        "altchar",        "will output one char of each starting with first char of file1", true, EnumSet.noneOf(RuleParam.class)),
-    _RANDOM_ALT_LINE("random-alt-line", "random-altline", "will output one line of each code randomly based on a seed for reproducability", true, EnumSet.of(RuleParam._SEED)),
-    _JOIN(           "join",            "join",            "will output merging of lines that have common occurrence", true, EnumSet.of(RuleParam._COL1, RuleParam._COL2)),
-    _ZIP(            "zip",             "zip",             "will output zip of the file", false, EnumSet.noneOf(RuleParam.class));
+    _1(              "1",               "1",               "will output file1", true, EnumSet.noneOf(RuleParam.class), Collections.emptyList()),
+    _2(              "2",               "2",               "will output file2", true, EnumSet.noneOf(RuleParam.class), Collections.emptyList()),
+    _ADD(            "+",               "add",             "will output file1+file2", true, EnumSet.noneOf(RuleParam.class), Collections.emptyList()),
+    _ALT_LINE(       "alt-line",        "altline",         "will output one line of each starting with first line of file1", true, EnumSet.noneOf(RuleParam.class), Collections.emptyList()), 
+    _ALT_CHAR(       "alt-char",        "altchar",         "will output one char of each starting with first char of file1", true, EnumSet.noneOf(RuleParam.class), Collections.emptyList()),
+    _RANDOM_ALT_LINE("random-alt-line", "random-altline",  "will output one line of each code randomly based on a seed for reproducability", true, EnumSet.of(RuleParam._SEED), Collections.singletonList("1789")),
+    _JOIN(           "join",            "join",            "will output merging of lines that have common occurrence", true, EnumSet.of(RuleParam._COL1, RuleParam._COL2), Arrays.asList("1", "2 1")),
+    _ZIP(            "zip",             "zip",             "will output zip of the file", false, EnumSet.noneOf(RuleParam.class), Collections.emptyList());
 
-  private final String name, extension, description;
+    private final String name, extension, description;
     private final boolean implemented;
     private final EnumSet<RuleParam> params;
+    private final List<String> testValues;
 
-    private Rule(String name, String extension, String description, boolean implemented, EnumSet<RuleParam> params) {
+    private Rule(String name, String extension, String description, boolean implemented, EnumSet<RuleParam> params, List<String> testValues) {
         this.name = name;
         this.extension = extension;
         this.description = description;
         this.implemented = implemented;
         this.params = params;
+        this.testValues = testValues;
     }
 
     /**
@@ -65,11 +70,18 @@ public enum Rule {
 
     /**
     * Returns an iterator over the additional parameters in this rule.
-    * @param name The name of the rule in command line
     * @return An iterator over the additional parameters in this rule
     */ 
     public Iterable<RuleParam> getParams() {
         return this.params;
+    }
+
+    /**
+    * Returns an iterator over the additional parameters values for testing
+    * @return An iterator over the additional parameters values for testing
+    */ 
+    public Iterable<String> getParamTestValues() {
+        return this.testValues;
     }
 
     /**
