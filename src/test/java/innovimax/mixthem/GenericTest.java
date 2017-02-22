8fc3e8a0f98f9ae80a5dba46434367919d60147e
Innovimax team <kojimch@gmail.com>
2017-02-22 12:47:24 +0100

    Update GenericTest.java
    add rule runs

--- src/test/java/innovimax/mixthem/GenericTest.java
+++ src/test/java/innovimax/mixthem/GenericTest.java
@@ -23,6 +23,7 @@ public class GenericTest {
 	   
 	   int i = 1;
 	   boolean result = true;
+	   RuleRuns ruleRuns = new RuleRuns();
 	   while (true) {
 		   System.out.println("TEST n°" + i);
 		   String prefix = "test" + String.format("%03d", i) +"_";
@@ -35,19 +36,24 @@ public class GenericTest {
 			   URL url = getClass().getResource(resource);
 			   System.out.println(rule+" implemented = "+rule.isImplemented()+" ; resource ("+resource+") = "+url);
 			   if (rule.isImplemented() && url != null) {
-			   	boolean res = check(new File(url1.getFile()), new File(url2.getFile()), new File(url.getFile()), rule);
+			   	boolean res = check(new File(url1.getFile()), new File(url2.getFile()), new File(url.getFile()), rule, Collections.emptyList());
 				System.out.println("RULE PASS : "+res);
 				result &= res;   
+				List<List<String>> runs = ruleRuns.getRuns(rule);
+				for (List<String> params : runs) {
+					boolean res2 = check(new File(url1.getFile()), new File(url2.getFile()), new File(url.getFile()), rule, params);
+					System.out.println("RULE PASS : "+res + " " + params.toArray().toString());
+				}
 			   }
 		   }   
 		   i++;
 	   }
 	   Assert.assertTrue(result);
    }	   
-   private final static boolean check(File file1, File file2, File expected, Rule rule)  throws MixException, FileNotFoundException, IOException  {
+   private final static boolean check(File file1, File file2, File expected, Rule rule, List<String> params)  throws MixException, FileNotFoundException, IOException  {
 	   ByteArrayOutputStream baos_rule = new ByteArrayOutputStream();
 	   MixThem mixThem = new MixThem(file1, file2, baos_rule);
-           mixThem.process(rule, Collections.emptyList());
+           mixThem.process(rule, params);
 	   return checkFileEquals(expected, baos_rule.toByteArray());
    }
 
