14125a3d53da9c85ae922d3c17fcdd30875dd334
Innovimax team <kojimch@gmail.com>
2017-02-22 18:11:16 +0100

    Update IJoinLine.java
    add methods for common field

--- src/main/java/innovimax/mixthem/interfaces/IJoinLine.java
+++ src/main/java/innovimax/mixthem/interfaces/IJoinLine.java
@@ -9,11 +9,30 @@ import innovimax.mixthem.exceptions.MixException;
 */
 public interface IJoinLine {
 	/**
- 	* Joins two lines on a common field.
+ 	* Joins two lines on the default first field.
  	* @param line1 The first line to be joined
  	* @param line2 The second line to be joined
  	* @return The result of joining lines, or null if no join possible
  	* @throws MixException - If an joining error occurs
  	*/	
 	String join(String line1, String line2) throws MixException;
+	/**
+ 	* Joins two lines on a common field
+ 	* @param line1 The first line to be joined
+ 	* @param line2 The second line to be joined
+	* @param index The index of the common field
+ 	* @return The result of joining lines, or null if no join possible
+ 	* @throws MixException - If an joining error occurs
+ 	*/	
+	String join(String line1, String line2, int index) throws MixException;
+	/**
+ 	* Joins two lines on a common field that has a different index.
+ 	* @param line1 The first line to be joined
+ 	* @param line2 The second line to be joined
+	* @param index1 The index of the common field in the first line
+	* @param index2 The index of the common field in the second line
+ 	* @return The result of joining lines, or null if no join possible
+ 	* @throws MixException - If an joining error occurs
+ 	*/	
+	String join(String line1, String line2, int index1, int index2) throws MixException;
 }
