20a0dc347f994fc8552ee357f3ddc9fd3cad1553
Innovimax team <kojimch@gmail.com>
2017-02-22 12:14:54 +0100

    Update RuleRuns.java
    add runs for rules

--- src/test/java/innovimax/mixthem/RuleRuns.java
+++ src/test/java/innovimax/mixthem/RuleRuns.java
@@ -2,6 +2,9 @@ package innovimax.mixthem;
 
 import innovimax.mixthem.arguments.Rule;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +19,26 @@ public class RuleRuns {
 
 	public RuleRuns() {
 		runMap = new HashMap<Rule, List<List<String>>>();
+		for (Rule rule : Rule.values()) {
+			List<List<String>> runs = new ArrayList<List<String>>();
+			if (rule.hasParams()) {
+				switch (rule) {
+                	case _RANDOM_ALT_LINE:                		
+                		runs.add(Collections.emptyList());
+                		runs.add(Collections.singletonList("1789"));                		
+                		break;
+                	case _JOIN:                  		
+                		runs.add(Collections.emptyList());
+                		runs.add(Collections.singletonList("1"));
+                		runs.add(Arrays.asList("2", "1"));            
+                		break;
+                }
+            } else {
+            	runs.add(Collections.emptyList());
+            	
+			}
+			runMap.put(rule, runs);
+		}
 	}
 
     public List<List<String>> getRuns(Rule rule) {    	    	
