2c3cc8ccc37ab666a9297f864bf5471d643e7255
Innovimax team <kojimch@gmail.com>
2017-02-22 16:30:45 +0100

    Update RuleRuns.java
    try to make source more pretty

--- src/test/java/innovimax/mixthem/RuleRuns.java
+++ src/test/java/innovimax/mixthem/RuleRuns.java
@@ -15,33 +15,33 @@ import java.util.Map;
 */
 public class RuleRuns {
 
-	final private Map<Rule, List<List<String>>> runMap;
+  final private Map<Rule, List<List<String>>> runMap;
 
-	public RuleRuns() {
-		runMap = new HashMap<Rule, List<List<String>>>();
-		for (Rule rule : Rule.values()) {
-			List<List<String>> runs = new ArrayList<List<String>>();
-			if (rule.hasParams()) {
-				switch (rule) {
-                	case _RANDOM_ALT_LINE:                		
-                		runs.add(Collections.emptyList());
-                		runs.add(Collections.singletonList("1789"));                		
-                		break;
-                	case _JOIN:                  		
-                		runs.add(Collections.emptyList());
-                		runs.add(Collections.singletonList("1"));
-                		runs.add(Arrays.asList("2", "1"));            
-                		break;
-                }
-            } else {
-            	runs.add(Collections.emptyList());            	
-			}
-			runMap.put(rule, runs);
-		}
+  public RuleRuns() {
+    runMap = new HashMap<Rule, List<List<String>>>();
+    for (Rule rule : Rule.values()) {
+      List<List<String>> runs = new ArrayList<List<String>>();
+      if (rule.hasParams()) {
+        switch (rule) {
+          case _RANDOM_ALT_LINE:
+            runs.add(Collections.emptyList());
+            runs.add(Collections.singletonList("1789"));
+            break;
+          case _JOIN:
+            runs.add(Collections.emptyList());
+            runs.add(Collections.singletonList("1"));
+            runs.add(Arrays.asList("2", "1"));
+            break;
 	}
-
-    public List<List<String>> getRuns(Rule rule) {    	    	
-    	return runMap.get(rule);
+      } else {
+        runs.add(Collections.emptyList());
+      }
+      runMap.put(rule, runs);
     }
+  }
+
+  public List<List<String>> getRuns(Rule rule) {
+    return runMap.get(rule);
+  }
 
 }
