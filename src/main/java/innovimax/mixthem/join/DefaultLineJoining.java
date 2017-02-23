481d44552429cf4dcdfc56fb4f6283e05d44c264
Innovimax team <kojimch@gmail.com>
2017-02-23 12:56:39 +0100

    Update DefaultLineJoining.java
    remove deprecated methods

--- src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
@@ -15,42 +15,7 @@ import java.util.stream.Collectors;
 * @version 1.0
 */
 public class DefaultLineJoining implements IJoinLine {
-/*
-	@Override
-	public String join(String line1, String line2) throws MixException {
-		if (line1 != null && line2 != null) {
-			List<String> list1 = Arrays.asList(line1.split("\\s"));
-			List<String> list2 = Arrays.asList(line2.split("\\s"));
-			if (list1.size() > 0 && list2.contains(list1.get(0))) {
-				String part1 = list1.stream().collect(Collectors.joining(" "));
-				String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
-				return part1 + " " + part2;				
-			}
-		}
-		return null;
-	}
-
-	@Override
-	public String join(String line1, String line2, int index) throws MixException {
-		if (line1 != null && line2 != null) {
-			List<String> list1 = Arrays.asList(line1.split("\\s"));
-			List<String> list2 = Arrays.asList(line2.split("\\s"));
-			if (list1.size() >= index && list2.size() >= index && list1.get(index - 1).equals(list2.get(index - 1))) {
-				String part1 = list1.get(index - 1);
-				String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
-				String part3 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
-				return part1 + " " + part2 + " " + part3;				
-			}
-		}
-		return null;
-	}
-
-	@Override
-	public String join(String line1, String line2, int index1, int index2) throws MixException {
-		System.out.println("join col1 col2 todo");
-		return null;
-	}
-*/	
+	
 	@Override
 	public String join(String line1, String line2, JoinType type, List<String> params) throws MixException {
 		String join = null;
@@ -66,7 +31,12 @@ public class DefaultLineJoining implements IJoinLine {
 					}				
 					break;
 				case _SAME_COL:		
-					int index = new Integer(params.get(0)).intValue();
+					int index;
+					try {
+						index = Integer.parseInt(params.get(0);
+					} catch (Exception NumberFormatException e) {
+						throw new MixException(e);
+					}
 					if (list1.size() >= index && list2.size() >= index && list1.get(index - 1).equals(list2.get(index - 1))) {
 						String part1 = list1.get(index - 1);
 						String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
