42721da013d28fab9b6af45af2c34b02abf49610
Innovimax team <kojimch@gmail.com>
2017-02-23 10:09:16 +0100

    Update DefaultLineJoining.java
    fix pmd

--- src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
@@ -35,13 +35,11 @@ public class DefaultLineJoining implements IJoinLine {
 		if (line1 != null && line2 != null) {
 			List<String> list1 = Arrays.asList(line1.split("\\s"));
 			List<String> list2 = Arrays.asList(line2.split("\\s"));
-			if (list1.size() <= index && list2.size() <= index) {
-				if (list1.get(index).equals(list2.get(index))) {
-					String part1 = list1.get(index);					
-					String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
-					String part3 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
-					return part1 + " " + part2 + " " + part3;	
-				}
+			if (list1.size() <= index && list2.size() <= index && list1.get(index).equals(list2.get(index))) {
+				String part1 = list1.get(index);					
+				String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
+				String part3 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+				return part1 + " " + part2 + " " + part3;				
 			}
 		}
 		return null;
