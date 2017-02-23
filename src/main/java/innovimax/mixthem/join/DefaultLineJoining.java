0115aa911a6680a7500c063d88069d88e6531f25
Innovimax team <kojimch@gmail.com>
2017-02-23 12:19:34 +0100

    Update DefaultLineJoining.java
    implement new method

--- src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
@@ -53,8 +53,32 @@ public class DefaultLineJoining implements IJoinLine {
 	
 	@Override
 	public String join(String line1, String line2, JoinType type, List<String> params) throws MixException {
-		System.out.println("join with enum todo");
-		return null;
+		String join = null;
+		if (line1 != null && line2 != null) {
+			List<String> list1 = Arrays.asList(line1.split("\\s"));
+			List<String> list2 = Arrays.asList(line2.split("\\s"));
+			switch (type) {
+				case _DEFAULT:				
+					if (list1.size() > 0 && list2.contains(list1.get(0))) {
+						String part1 = list1.stream().collect(Collectors.joining(" "));
+						String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+						join = part1 + " " + part2;
+					}				
+					break;
+				case _SAME_COL:			
+					if (list1.size() >= index && list2.size() >= index && list1.get(index - 1).equals(list2.get(index - 1))) {
+						String part1 = list1.get(index - 1);
+						String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
+						String part3 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+						join = part1 + " " + part2 + " " + part3;				
+					}
+					break;
+				case _DIFF_COL:
+					System.out.println("join col1 col2 todo");
+					break;
+			}
+		}
+		return join;
 	}
 	
 }
