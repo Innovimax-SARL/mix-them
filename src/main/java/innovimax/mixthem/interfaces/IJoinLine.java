26d8e34a893417108139b2218804665f6687dd4c
Innovimax team <kojimch@gmail.com>
2017-02-23 17:22:55 +0100

    add javadoc, update method twrowing

--- src/main/java/innovimax/mixthem/interfaces/IJoinLine.java
+++ src/main/java/innovimax/mixthem/interfaces/IJoinLine.java
@@ -11,6 +11,20 @@ import java.util.List;
 * @version 1.0
 */
 public interface IJoinLine {	
+	/**
+ 	* Returns the result of joining two lines depends on type and additional parameters.
+	* @param line1 The first line to join
+ 	* @param line2 The second line to join
+	* @param type The type of join required
+	* @param params The list of additional parameters (from command line)
+ 	* @return The result of joining two lines depends on type and additional parameters
+ 	* @throws MixException - If an mixing error occurs
+ 	*/
 	String join(String line1, String line2, JoinType type, List<String> params) throws MixException;	
-	JoinType getType(List<String> params) throws MixException;
+	/**
+ 	* Retrieves the joining type regarding the additional parameters.
+ 	* @param params The list of additional parameters (from command line)
+ 	* @return The joining type regarding the additional parameters
+ 	*/
+	JoinType getType(List<String> params);
 }
