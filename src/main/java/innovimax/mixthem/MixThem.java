ef7ee9f0295fb3a5b3079558a087408406018095
Innovimax team <kojimch@gmail.com>
2017-02-24 12:15:00 +0100

    enable logger import

--- src/main/java/innovimax/mixthem/MixThem.java
+++ src/main/java/innovimax/mixthem/MixThem.java
@@ -10,7 +10,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
-//import java.util.logging.ConsoleHandler;
+import java.util.logging.ConsoleHandler;
 //import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
