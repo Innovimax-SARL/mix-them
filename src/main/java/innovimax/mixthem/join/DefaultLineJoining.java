d3b9a604a73c2545ee99cd44a82125135a09c2e2
Innovimax team <kojimch@gmail.com>
2017-02-24 09:23:02 +0100

    implement new join method from interface

--- src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/join/DefaultLineJoining.java
@@ -97,5 +97,43 @@ public class DefaultLineJoining implements IJoinLine {
 			throw new MixException("Unexpected join parameter values " + params.toString(), e);
 		}
 	}
+	
+	@Override
+	public String join(String line1, String line2, JoinType type, List<Integer> columns) throws MixException {
+		String join = null;
+		if (line1 != null && line2 != null) {
+			List<String> list1 = Arrays.asList(line1.split("\\s"));
+			List<String> list2 = Arrays.asList(line2.split("\\s"));
+			switch (type) {
+				case _DEFAULT:				
+					if (list1.size() > 0 && list2.contains(list1.get(0))) {
+						String part1 = list1.stream().collect(Collectors.joining(" "));
+						String part2 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+						join = part1 + " " + part2;
+					}				
+					break;
+				case _SAME_COL:		
+					int col = columns.get(0).intValue();
+					if (list1.size() >= index && list2.size() >= index && list1.get(index - 1).equals(list2.get(index - 1))) {
+						String part1 = list1.get(index - 1);
+						String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
+						String part3 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+						join = part1 + " " + part2 + " " + part3;				
+					}
+					break;
+				case _DIFF_COL:
+					int col1 = columns.get(0).intValue();
+					int col2 = columns.get(1).intValue();
+					if (list1.size() >= col1 && list2.size() >= col2 && list1.get(col1 - 1).equals(list2.get(col2 - 1))) {
+						String part1 = list1.get(col1 - 1);
+						String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
+						String part3 = list2.stream().filter(s -> !list1.contains(s)).collect(Collectors.joining(" "));
+						join = part1 + " " + part2 + " " + part3;				
+					}
+					break;
+			}
+		}
+		return join;
+	}
 
 }
