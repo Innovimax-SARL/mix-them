4c019a2976e5e3951bb87315d0278d6c6f2d65eb
Innovimax team <kojimch@gmail.com>
2017-02-24 09:46:55 +0100

    remove deprecated method

--- src/main/java/innovimax/mixthem/interfaces/IJoinLine.java
+++ src/main/java/innovimax/mixthem/interfaces/IJoinLine.java
@@ -12,16 +12,6 @@ import java.util.List;
 */
 public interface IJoinLine {	
 	/**
- 	* Returns the result of joining two lines depends on type and additional parameters.
-	* @param line1 The first line to join
- 	* @param line2 The second line to join
-	* @param type The type of join required
-	* @param params The list of additional parameters (from command line)
- 	* @return The result of joining two lines depends on type and additional parameters
- 	* @throws MixException - If an mixing error occurs
- 	*/
-	//String join(String line1, String line2, JoinType type, List<String> params) throws MixException;	
-	/**
  	* Retrieves the joining type regarding the additional parameters.
  	* @param params The list of additional parameters (from command line)
  	* @return The joining type regarding the additional parameters
