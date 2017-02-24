923e450f6a8da17503f980dc22c20a0949d36f9d
Innovimax team <kojimch@gmail.com>
2017-02-24 11:10:02 +0100

    add import

--- src/main/java/innovimax/mixthem/MixThem.java
+++ src/main/java/innovimax/mixthem/MixThem.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
