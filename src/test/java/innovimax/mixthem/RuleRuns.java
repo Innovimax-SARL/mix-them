6d461e13280becf29688362fdb5ed96f9af096de
Innovimax team <kojimch@gmail.com>
2017-02-27 17:36:54 +0100

    remove useless import

--- src/test/java/innovimax/mixthem/RuleRuns.java
+++ src/test/java/innovimax/mixthem/RuleRuns.java
@@ -9,13 +9,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 /**
