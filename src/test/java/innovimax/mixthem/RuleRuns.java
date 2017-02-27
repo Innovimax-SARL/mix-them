df4346648cc8be4a57adf2d1f00a573a1d796c72
Innovimax team <kojimch@gmail.com>
2017-02-27 17:17:17 +0100

    remove deprecated code

--- src/test/java/innovimax/mixthem/RuleRuns.java
+++ src/test/java/innovimax/mixthem/RuleRuns.java
@@ -27,49 +27,12 @@ public class RuleRuns {
 	
 	final private static String DEFAULT_OUTPUT_FILE = "default";
 
-  final private Map<Rule, List<RuleRun>> runMap;
-
-  /*
-  * Creates the rule run maker.
-  */
-  public RuleRuns() {
-    runMap = new HashMap<Rule, List<RuleRun>>();
-    for (Rule rule : Rule.values()) {
-      List<RuleRun> runs = new ArrayList<RuleRun>();
-      if (rule.hasParams()) {
-        switch (rule) {
-          case _RANDOM_ALT_LINE:
-            runs.add(new RuleRun(1, Collections.emptyList()));
-            runs.add(new RuleRun(1, Collections.singletonList("1789")));
-            break;
-          case _JOIN:
-	    runs.add(new RuleRun(3, "first", Collections.emptyList()));
-	    runs.add(new RuleRun(3, "same", Collections.singletonList("2")));
-            runs.add(new RuleRun(3, "diff", Arrays.asList("4", "3")));			
-            break;
-	}
-      } else {
-        runs.add(new RuleRun(Collections.emptyList()));
-      }
-      runMap.put(rule, runs);
-    }
-  }
-
-  /**
-  * Returns a list of test runs for the rule.
-  * @param rule The rule to test
-  * @return Returns a list of test runs for the rule
-    */	
-  public List<RuleRun> getRuns(Rule rule) {
-    return runMap.get(rule);
-  }
-
   	/**
   	* Returns a list of test runs for the rule.
   	* @param url The URL of rule additional parameters file
   	* @return Returns a list of test runs for the rule
     	*/	
-	public List<RuleRun> getRuns(URL url) throws FileNotFoundException, IOException {
+	public static List<RuleRun> getRuns(URL url) throws FileNotFoundException, IOException {
     		List<RuleRun> runs = new LinkedList<RuleRun>();
     		runs.add(new RuleRun(Collections.emptyList()));
 		if (url != null) {
