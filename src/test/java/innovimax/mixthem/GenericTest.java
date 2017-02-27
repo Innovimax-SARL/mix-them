4493ecaaa9ceacf31b7aa8b1b35e6dd15794eff4
Innovimax team <kojimch@gmail.com>
2017-02-27 17:57:08 +0100

    small fix

--- src/test/java/innovimax/mixthem/GenericTest.java
+++ src/test/java/innovimax/mixthem/GenericTest.java
@@ -30,8 +30,8 @@ public class GenericTest {
 		   String prefix = "test" + String.format("%03d", testId) +"_";
 		   URL url1 = getClass().getResource(prefix + "file1.txt");
 		   URL url2 = getClass().getResource(prefix + "file2.txt");
-		   MixThem.LOGGER.fine("--> URL 1 (" + prefix + "file1.txt"+") : " + (url1 == null ? "FOUND" : "NOT FOUND"));
-		   MixThem.LOGGER.fine("--> URL 2 (" + prefix + "file2.txt"+") : " + (url2 == null ? "FOUND" : "NOT FOUND"));
+		   MixThem.LOGGER.fine("--> URL 1 (" + prefix + "file1.txt"+") : " + (url1 != null ? "FOUND" : "NOT FOUND"));
+		   MixThem.LOGGER.fine("--> URL 2 (" + prefix + "file2.txt"+") : " + (url2 != null ? "FOUND" : "NOT FOUND"));
 		   if( url1 == null || url2 == null) break;
 		   for(Rule rule : Rule.values()) {
 			   MixThem.LOGGER.info("RULE " + rule + " (" + (rule.isImplemented() ? "" : "NOT ") + "IMPLEMENTED)");
@@ -49,7 +49,7 @@ public class GenericTest {
 					   }
 					   resultFile += ".txt";
 					   URL urlR = getClass().getResource(resultFile);
-					   MixThem.LOGGER.fine("--> Result (" + resultFile + ") : " + (urlR == null ? "FOUND" : "NOT FOUND"));
+					   MixThem.LOGGER.fine("--> Result (" + resultFile + ") : " + (urlR != null ? "FOUND" : "NOT FOUND"));
 					   if (urlR != null) {
 						   boolean res = check(new File(url1.getFile()), new File(url2.getFile()), new File(urlR.getFile()), rule, run.getParams());
 						   MixThem.LOGGER.info("RUN " + (res ? "PASS" : "FAIL") + " WITH PARAMS " + run.getParams().toString());
