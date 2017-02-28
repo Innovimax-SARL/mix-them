ce7faea35f7811ee2ba74041f99f247a29c96896
Innovimax team <kojimch@gmail.com>
2017-02-28 13:56:08 +0100

    Create DefaultLineZipping.java

---
+++ src/main/java/innovimax/mixthem/operation/DefaultLineZipping.java
@@ -0,0 +1,24 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.exceptions.MixException;
+import innovimax.mixthem.interfaces.IZipLine;
+
+/**
+* <p>Joins two lines on a common field.</p>
+* <p>This is the default implementation of IJoinLine.</p>
+* @see IJoinLine
+* @author Innovimax
+* @version 1.0
+*/
+public class DefaultLineZipping implements IZipLine {
+
+	@Override
+	public String zip(String line1, String line2) throws MixException {
+		String zippedLine = null;
+		if (line1 != null && line2 != null) {
+      //TODO
+		}
+		return zippedLine;
+	}
+
+}
