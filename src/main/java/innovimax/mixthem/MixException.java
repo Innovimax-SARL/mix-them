2e83ad1225d0baafa04e66406496e45027973a97
Innovimax team <kojimch@gmail.com>
2017-02-28 15:54:38 +0100

    remove useless constructors

--- src/main/java/innovimax/mixthem/MixException.java
+++ src/main/java/innovimax/mixthem/MixException.java
@@ -7,18 +7,6 @@ package innovimax.mixthem;
 */
 public class MixException extends Exception {
 
-	public MixException() { 
-		super();
-	}
-	
-	public MixException(String message) { 
-		super(message); 
-	}
-	
-	public MixException(Throwable cause) { 
-		super(cause); 
-	}
-
 	public MixException(String message, Throwable cause) {
 		super(message, cause);
 	}
