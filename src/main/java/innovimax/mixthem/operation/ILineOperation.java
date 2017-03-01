b61f8a0e27a7f6911a398999be822f49d31c057b
Innovimax <innovimax@gmail.com>
2017-03-01 23:36:49 +0100

    Update ILineOperation.java

--- src/main/java/innovimax/mixthem/operation/ILineOperation.java
+++ src/main/java/innovimax/mixthem/operation/ILineOperation.java
@@ -1,5 +1,6 @@
 package innovimax.mixthem.operation;
+import innovimax.mixthem.MixException;
 
 interface ILineOperation implements IOperation {
-	public String process(String line1, String line2);
+	public String process(String line1, String line2) throws MixException;
 }
