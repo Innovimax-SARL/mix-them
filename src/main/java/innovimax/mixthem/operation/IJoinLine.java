87e444db1c658b66eb989d36b41e1f6ccd4b3f22
Innovimax team <kojimch@gmail.com>
2017-03-01 11:53:25 +0100

    remove/update methods

--- src/main/java/innovimax/mixthem/operation/IJoinLine.java
+++ src/main/java/innovimax/mixthem/operation/IJoinLine.java
@@ -1,8 +1,11 @@
 package innovimax.mixthem.operation;
 
+import innovimax.mixthem.arguments.ParamValue;
+import innovimax.mixthem.arguments.RuleParam;
+
 import innovimax.mixthem.MixException;
 
-import java.util.List;
+import java.util.Map;
 
 /**
 * This interface provides for joining two lines
@@ -11,25 +14,14 @@ import java.util.List;
 */
 public interface IJoinLine {	
 	/**
- 	* Retrieves the joining type regarding the additional parameters.
- 	* @param params The list of additional parameters (from command line)
- 	* @return The joining type regarding the additional parameters
- 	*/
-	JoinType getType(List<String> params);
-	/**
- 	* Returns the list of column parameters as integer.
- 	* @param params The list of additional parameters (from command line)
- 	* @return The list of column parameters as integer
- 	*/
-	List<Integer> getColumns(List<String> params) throws MixException;
-	/**
- 	* Returns the result of joining two lines regarding the type of join.
+ 	* Returns the result of joining two lines regarding the parameters.
 	* @param line1 The first line to join
  	* @param line2 The second line to join
-	* @param type The type of join required
-	* @param columns The list of column indexes depends on type (maybe empty)
- 	* @return The result of joining two lines regarding the type of join
+	* @param params The list of parameters (maybe empty)
+ 	* @return The result of joining two lines regarding the parameters
  	* @throws MixException - If an mixing error occurs
+	* @see innovimax.mixthem.operation.RuleParam
+	* @see innovimax.mixthem.operation.ParamValue
  	*/
-	String join(String line1, String line2, JoinType type, List<Integer> columns) throws MixException;	
+	String join(String line1, String line2, Map<RuleParam, ParamValue> params) throws MixException;	
 }
