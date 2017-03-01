fac0d7ba709fa5283e8c7355ff495bab808b46cd
Innovimax <innovimax@gmail.com>
2017-03-01 23:28:49 +0100

    Update DefaultLineJoining.java

--- src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
@@ -18,6 +18,16 @@ import java.util.stream.Collectors;
 */
 public class DefaultLineJoining implements IJoinLine {
 
+	/**
+ 	* Returns the result of joining two lines regarding the parameters.
+	* @param line1 The first line to join
+ 	* @param line2 The second line to join
+	* @param params The list of parameters (maybe empty)
+ 	* @return The result of joining two lines regarding the parameters
+ 	* @throws MixException - If an mixing error occurs
+	* @see innovimax.mixthem.operation.RuleParam
+	* @see innovimax.mixthem.operation.ParamValue
+ 	*/
 	@Override
 	public String join(String line1, String line2, Map<RuleParam, ParamValue> params) throws MixException {
 		String join = null;
