59afc8a6f9677c28c13b8ae4dcb384cf320e5fb4
Innovimax team <kojimch@gmail.com>
2017-03-01 11:32:01 +0100

    enable code

--- src/main/java/innovimax/mixthem/arguments/ParamValue.java
+++ src/main/java/innovimax/mixthem/arguments/ParamValue.java
@@ -8,24 +8,24 @@ package innovimax.mixthem.arguments;
 */
 public class ParamValue { 
 	
-	//private final String text;
+	private final String text;
 	private final int number;
 	
 	/**
 	* Constructor
 	* @param text The String value of the parameter.	
 	*/ 	
-	/*public ParamValue(String text) {
+	public ParamValue(String text) {
 		this.text = test;
 		this.number = -1;
-	}*/
+	}
 
 	/**
 	* Constructor
 	* @param number The Integer value of the parameter.	
 	*/ 	
 	public ParamValue(int number) {
-		//this.text = null;
+		this.text = null;
 		this.number = number;
 	}
 	
@@ -33,9 +33,9 @@ public class ParamValue {
 	* Returns the parameter value as a String.
 	* @return The parameter value as a String
 	*/
-	/*public String stringValue() {
+	public String stringValue() {
 		return this.text;
-	}*/
+	}
 
 	/**
 	* Returns the parameter value as an Integer.
