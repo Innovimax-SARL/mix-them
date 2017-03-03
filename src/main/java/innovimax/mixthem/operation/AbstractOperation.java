7d30ac3279d6654ad96eaedf4fd92c2a9d0f3989
Innovimax team <kojimch@gmail.com>
2017-03-03 16:46:22 +0100

    Create AbstractOperation.java

---
+++ src/main/java/innovimax/mixthem/operation/AbstractOperation.java
@@ -0,0 +1,28 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.arguments.RuleParam;
+import innovimax.mixthem.arguments.ParamValue;
+
+import java.util.Map;
+
+/**
+* <p>Abstract class for all operation.</p>
+* @see ILineOperation
+* @author Innovimax
+* @version 1.0
+*/
+public class AbstractOperation implements ILineOperation {
+	
+	protected final Map<RuleParam, ParamValue> params;
+	
+	/**
+	* Constructor
+ 	* @param params The list of parameters (maybe empty)
+	* @see innovimax.mixthem.arguments.RuleParam
+	* @see innovimax.mixthem.arguments.ParamValue
+	*/
+	public AbstractOperation(Map<RuleParam, ParamValue> params) {
+		this.params = params;
+	}
+
+}
