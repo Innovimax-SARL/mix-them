ea13a8edaac16d28e4a365c3c7e6f9cbdd1ca25a
Innovimax team <kojimch@gmail.com>
2017-03-03 16:19:57 +0100

    fix zip-cell process

--- src/main/java/innovimax/mixthem/operation/DefaultLineZipping.java
+++ src/main/java/innovimax/mixthem/operation/DefaultLineZipping.java
@@ -60,6 +60,8 @@ public class DefaultLineZipping implements ILineOperation {
 					while (index < list1.size() && index < list2.size()) {						
 						if (index == 0) {
 							zip = "";
+						} else {
+							zip += " ";  // cell separator
 						}
 						zip += list1.get(index) + sep + list2.get(index);
 						index++;
