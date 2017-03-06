b9b6ba642a220202d9c8e1c1c825a1bf7a30e025
Innovimax team <kojimch@gmail.com>
2017-03-06 10:39:02 +0100

    Create AbstractCharOperation.java

---
+++ src/main/java/innovimax/mixthem/operation/AbstractCharOperation.java
@@ -0,0 +1,26 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.arguments.RuleParam;
+import innovimax.mixthem.arguments.ParamValue;
+
+import java.util.Map;
+
+/**
+* <p>Abstract class for all line operation.</p>
+* @see ILineOperation
+* @author Innovimax
+* @version 1.0
+*/
+public abstract class AbstractCharOperation extends AbstractOperation implements ICharOperation {
+
+	/**
+	* Constructor
+ 	* @param params The list of parameters (maybe empty)
+	* @see innovimax.mixthem.arguments.RuleParam
+	* @see innovimax.mixthem.arguments.ParamValue
+	*/
+	public AbstractLineOperation(Map<RuleParam, ParamValue> params) {
+		super(params);
+	}
+
+}
