ae636a7746cccd6d971abe11f47028e5fe9601a3
Innovimax team <kojimch@gmail.com>
2017-03-06 09:31:16 +0100

    zip chars return int[]

--- src/main/java/innovimax/mixthem/operation/ICharOperation.java
+++ src/main/java/innovimax/mixthem/operation/ICharOperation.java
@@ -3,5 +3,5 @@ package innovimax.mixthem.operation;
 import innovimax.mixthem.MixException;
 
 public interface ICharOperation extends IOperation {
-	String process(int c1, int c2) throws MixException;
+	int[] process(int c1, int c2) throws MixException;
 }
