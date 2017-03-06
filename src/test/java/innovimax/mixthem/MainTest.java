def49c3f7ff08faf18db1a1275813abf168f17ff
Innovimax team <kojimch@gmail.com>
2017-03-06 17:32:08 +0100

    move test in main tests

--- src/test/java/innovimax/mixthem/MainTest.java
+++ src/test/java/innovimax/mixthem/MainTest.java
@@ -2,6 +2,13 @@ package innovimax.mixthem;
 
 import innovimax.mixthem.arguments.ArgumentException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,17 +19,33 @@ import org.junit.Test;
 public class MainTest {
 
     @Test
-    public final void testMainEmptyArgs() throws ArgumentException {
+    public final void testMainEmptyArgs() {
         final String args[] = {};
         MixThem.main(args);
         Assert.assertTrue(true);
     }
     
     @Test
-    public final void testMainRule1() throws ArgumentException {
+    public final void testMainRule1() {
         final String args[] = { "-1", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
         MixThem.main(args);
         Assert.assertTrue(true);
     }
-
+    
+    @Test
+    public final void testMainRule1Lock() {
+        try {
+            File file = new File(getClass().getResource("test001_file1.txt").getFile());
+            FileChannel fileChannel = new RandomAccessFile(file, "rw").getChannel();
+            FileLock lock = fileChannel.lock();
+            final String args[] = { "-1", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
+            MixThem.main(args);
+            Assert.assertTrue(true);
+        } catch (FileNotFoundException e) {
+            Assert.assertTrue(false);
+        } catch (IOException e) {
+            Assert.assertTrue(false);
+        }
+    }
+    
 }
