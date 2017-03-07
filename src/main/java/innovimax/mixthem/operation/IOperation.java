3dde5b30a0ab471790471d7d3743bfed9d512583
Innovimax team <kojimch@gmail.com>
2017-03-07 11:26:47 +0100

    add processFiles to operation interface

--- src/main/java/innovimax/mixthem/operation/IOperation.java
+++ src/main/java/innovimax/mixthem/operation/IOperation.java
@@ -1,4 +1,11 @@
 package innovimax.mixthem.operation;
 
+import innovimax.mixthem.MixException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public interface IOperation {
+  void processFiles(File file1, File file2, OutputStream out) throws MixException, IOException;
 }
