b2f199edf8cc557916d93779a3d44014c951fcda
Innovimax team <kojimch@gmail.com>
2017-03-07 11:57:31 +0100

    remove public

--- src/main/java/innovimax/mixthem/operation/ICharOperation.java
+++ src/main/java/innovimax/mixthem/operation/ICharOperation.java
@@ -2,6 +2,6 @@
 package innovimax.mixthem.operation;
 import innovimax.mixthem.MixException;
 
-public interface ICharOperation extends IOperation {
+interface ICharOperation extends IOperation {
 	int[] process(int c1, int c2) throws MixException;
 }
