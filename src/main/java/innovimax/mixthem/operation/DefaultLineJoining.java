a2dad9565e6bd8ca5de9b542a0eb18fde2d4ec6c
Innovimax team <kojimch@gmail.com>
2017-03-07 15:22:02 +0100

    update join operation

--- src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
@@ -27,33 +27,37 @@ public class DefaultLineJoining extends AbstractLineOperation {
 	}	
 
 	/**
- 	* Returns the result of joining two lines regarding the parameters.
+ 	* Returns the result of joining two lines regarding the parameters or null if none.
 	* @param line1 The first line to join
  	* @param line2 The second line to join
- 	* @return The result of joining two lines regarding the parameters
+ 	* @return The result of joining two lines regarding the parameters or null if none
  	* @throws MixException - If an mixing error occurs
  	*/
 	@Override
 	public String process(String line1, String line2) throws MixException {
-		String join = null;
-		int col1 = 1;
-		int col2 = 1;
-		if (this.params.containsKey(RuleParam._JOIN_COL1)) {
-			col1 = this.params.get(RuleParam._JOIN_COL1).asInt();
-			col2 = col1;
+		if (line1 == null || line2 == null) {
+			return null;
+		} else {
+			String join = null;
+			int col1 = 1;
+			int col2 = 1;
+			if (this.params.containsKey(RuleParam._JOIN_COL1)) {
+				col1 = this.params.get(RuleParam._JOIN_COL1).asInt();
+				col2 = col1;
+			}
+			if (this.params.containsKey(RuleParam._JOIN_COL2)) {
+				col2 = this.params.get(RuleParam._JOIN_COL2).asInt();			
+			}
+			List<String> list1 = Arrays.asList(line1.split("\\s"));
+			List<String> list2 = Arrays.asList(line2.split("\\s"));		
+			if (list1.size() >= col1 && list2.size() >= col2 && list1.get(col1 - 1).equals(list2.get(col2 - 1))) {
+				String part1 = list1.get(col1 - 1);
+				String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
+				String part3 = list2.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
+				join = part1 + " " + part2 + " " + part3;				
+			}		
+			return join;
 		}
-		if (this.params.containsKey(RuleParam._JOIN_COL2)) {
-			col2 = this.params.get(RuleParam._JOIN_COL2).asInt();			
-		}
-		List<String> list1 = Arrays.asList(line1.split("\\s"));
-		List<String> list2 = Arrays.asList(line2.split("\\s"));		
-		if (list1.size() >= col1 && list2.size() >= col2 && list1.get(col1 - 1).equals(list2.get(col2 - 1))) {
-			String part1 = list1.get(col1 - 1);
-			String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
-			String part3 = list2.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(" "));
-			join = part1 + " " + part2 + " " + part3;				
-		}		
-		return join;
 	}
 
 }
