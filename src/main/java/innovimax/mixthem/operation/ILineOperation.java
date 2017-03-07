12bcd3c64b0cb14e80365d1c7999e878ed34fb70
Innovimax team <kojimch@gmail.com>
2017-03-07 16:24:44 +0100

    update javadoc

--- src/main/java/innovimax/mixthem/operation/ILineOperation.java
+++ src/main/java/innovimax/mixthem/operation/ILineOperation.java
@@ -2,5 +2,13 @@ package innovimax.mixthem.operation;
 import innovimax.mixthem.MixException;
 
 interface ILineOperation extends IOperation {
+	/**
+ 	* Returns the result of the operation (maybe null).
+	* @param line1 The first line to join
+ 	* @param line2 The second line to join
+ 	* @return The result result of the operation (maybe null)
+ 	* @throws MixException - If an mixing error occurs
+	* @throws ProcessException - If process must be ended
+ 	*/
 	String process(String line1, String line2) throws MixException, ProcessException;
 }
