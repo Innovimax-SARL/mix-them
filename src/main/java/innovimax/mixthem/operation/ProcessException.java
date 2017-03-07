80128b4934c065e9e415ed7f248873318882a328
Innovimax team <kojimch@gmail.com>
2017-03-07 16:13:53 +0100

    Create ProcessException.java

---
+++ src/main/java/innovimax/mixthem/operation/ProcessException.java
@@ -0,0 +1,14 @@
+package innovimax.mixthem.arguments;
+
+/**
+* Indicates that a process must be stopped.
+* @author Innovimax
+* @version 1.0
+*/
+public class ProcessException extends Exception {
+
+	public ProcessException() { 
+		super(); 
+	}
+	
+}
