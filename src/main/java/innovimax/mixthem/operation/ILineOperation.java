43bccdf219914c86d7cfc74924b63159636bfe5a
Innovimax team <kojimch@gmail.com>
2017-03-07 11:52:30 +0100

    remove public

--- src/main/java/innovimax/mixthem/operation/ILineOperation.java
+++ src/main/java/innovimax/mixthem/operation/ILineOperation.java
@@ -1,6 +1,6 @@
 package innovimax.mixthem.operation;
 import innovimax.mixthem.MixException;
 
-public interface ILineOperation extends IOperation {
+interface ILineOperation extends IOperation {
 	String process(String line1, String line2) throws MixException;
 }
