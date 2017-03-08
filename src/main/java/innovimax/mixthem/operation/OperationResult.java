176f1e79816ebebde4a52fe7760898f90958081a
Innovimax team <kojimch@gmail.com>
2017-03-08 16:08:56 +0100

    small fix

--- src/main/java/innovimax/mixthem/operation/OperationResult.java
+++ src/main/java/innovimax/mixthem/operation/OperationResult.java
@@ -12,22 +12,22 @@ class OperationResult {
   private final ResultType type;
   private final char[] data;  
   
-	public OperationResult(char[] data) {
-    this.type = FILL;
+  public OperationResult(char[] data) {
+    this.type = ResultType.FILL;
     this.data = data;    
   }
 
-	public OperationResult(ResultType type) {
+  public OperationResult(ResultType type) {
     this.type = type;
     this.data = null;
   }
   
   public boolean isEmpty() {
-    return this.type == EMPTY;
+    return this.type == ResultType.EMPTY;
   }
 	
   public boolean wantStop() {
-    return this.type == STOP;
+    return this.type == ResultType.STOP;
   }
   
   public char[] getData() {
