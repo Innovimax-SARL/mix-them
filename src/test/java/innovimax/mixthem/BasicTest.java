0f428b497bc3280cb8b4bf954ce90db7fec6850c
Innovimax team <kojimch@gmail.com>
2017-03-08 12:20:40 +0100

    Update BasicTest.java

--- src/test/java/innovimax/mixthem/BasicTest.java
+++ src/test/java/innovimax/mixthem/BasicTest.java
@@ -31,39 +31,45 @@ public class BasicTest {
 
     @Test
     public final void testNoRule() throws ArgumentException {
-        final String args[] = { getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
+        final String args[] = { getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
         Assert.assertTrue(true);
     }
     @Test
     public final void test1Rule() throws ArgumentException {
-        final String args[] = { "-1", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
+        final String args[] = { "-1", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
         Assert.assertTrue(true);
     }
     @Test(expected=ArgumentException.class)
     public final void testUnknownRule() throws ArgumentException {
-        final String args[] = { "-x", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
+        final String args[] = { "-x", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
     }
 
     @Test(expected=ArgumentException.class)
     public final void testUnexpectedParam() throws ArgumentException {
-        final String args[] = { "-1", "#val", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
+        final String args[] = { "-1", "#val", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
     }
 
     @Test(expected=ArgumentException.class)
     public final void testWrongSeedParam() throws ArgumentException {
-        final String args[] = { "-random-alt-line", "#val", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
+        final String args[] = { "-random-alt-line", "#val", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
     }
 
     @Test
     public final void testValidSeedParam() throws ArgumentException {
-        final String args[] = { "-random-alt-line", "#1789", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file1.txt").getFile() };
+        final String args[] = { "-random-alt-line", "#1789", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
         Assert.assertTrue(true);        
+    }
+
+    @Test(expected=ArgumentException.class)
+    public final void testNotAParam() throws ArgumentException {
+        final String args[] = { "-random-alt-line", "1789", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
+        Arguments mixArgs = Arguments.checkArguments(args);
     }
 
 }
