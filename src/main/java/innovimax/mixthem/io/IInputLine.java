e80483058cdc4a862b8b6e11793db6d1d1c4c218
Innovimax team <kojimch@gmail.com>
2017-03-08 10:03:52 +0100

    remove reading type and seed

--- src/main/java/innovimax/mixthem/io/IInputLine.java
+++ src/main/java/innovimax/mixthem/io/IInputLine.java
@@ -9,23 +9,17 @@ import java.io.IOException;
 */
 public interface IInputLine {
 	/**
-	* Sets seed value for random reading only.
-	* @param The seed value	
-	*/	
-	void setSeed(int seed);
-	/**
 	* Returns true if there is more lines.
 	* @return Returns true if there is more lines
 	* @throws IOException - If an I/O error occurs
 	*/	
 	boolean hasLine() throws IOException;
 	/**
- 	* Reads an eligible line regarding the type, or returns null if not eligible or no more lines.
- 	* @param type The type of reading expected
- 	* @return The eligible line, or null if not eligible or no more lines
+ 	* Reads a line, or returns null if no more lines.
+ 	* @return The line, or null if no more lines
  	* @throws IOException - If an I/O error occurs
  	*/	
-	String nextLine(ReadType type) throws IOException;
+	String nextLine() throws IOException;
 	/**
 	* Closes this input and releases any system resources associated with it.
 	* @throws IOException - If an I/O error occurs
