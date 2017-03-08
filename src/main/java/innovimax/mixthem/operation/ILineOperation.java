132cb8c7fcfe807e4bd8e6132d1cd79a453b825a
Innovimax team <kojimch@gmail.com>
2017-03-08 16:16:28 +0100

    interface returns OperationResult

--- src/main/java/innovimax/mixthem/operation/ILineOperation.java
+++ src/main/java/innovimax/mixthem/operation/ILineOperation.java
@@ -3,12 +3,11 @@ import innovimax.mixthem.MixException;
 
 interface ILineOperation extends IOperation {
 	/**
- 	* Returns the result of the operation (maybe null).
+ 	* Returns the result of the operation as {@link OperationResult}.
 	* @param line1 The first line to join (maybe null)
  	* @param line2 The second line to join (maybe null)
- 	* @return The result result of the operation (maybe null)
+ 	* @return The result result of the operation as {@link OperationResult}
  	* @throws MixException - If an mixing error occurs
-	* @throws ProcessException - If process must be ended
  	*/
-	String process(String line1, String line2) throws MixException, ProcessException;
+	OperationResult process(String line1, String line2) throws MixException;
 }
