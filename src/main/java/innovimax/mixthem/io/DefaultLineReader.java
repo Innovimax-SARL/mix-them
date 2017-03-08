fab63c3dfd9a4615bb5492a496bbe51e9b14f237
Innovimax team <kojimch@gmail.com>
2017-03-08 10:12:26 +0100

    remove unused import

--- src/main/java/innovimax/mixthem/io/DefaultLineReader.java
+++ src/main/java/innovimax/mixthem/io/DefaultLineReader.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Random;
 
 /**
 * <p>Reads lines from a character-input file.</p>
