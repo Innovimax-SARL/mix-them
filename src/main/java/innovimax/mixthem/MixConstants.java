5c2ff071612c7882cf03f093e29e0c6998bdb364
Innovimax team <kojimch@gmail.com>
2017-03-09 12:24:51 +0100

    add mix constant

--- src/main/java/innovimax/mixthem/MixConstants.java
+++ src/main/java/innovimax/mixthem/MixConstants.java
@@ -2,6 +2,7 @@ package innovimax.mixthem;
 
 public final class MixConstants {
   public final static int CHAR_BUFFER_SIZE = 1024;
+  public final static int DEFAULT_RANDOM_SEED = 1789;
   public final static int DEFAULT_JOIN_COLUMN = 1;
   public final static String DEFAULT_ZIP_SEPARATOR = "";
   public final static String DEFAULT_SPLIT_CELL_REGEX = "\\s";
