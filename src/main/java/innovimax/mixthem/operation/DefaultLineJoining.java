ea10be67649c861f33b533dd60ab4933a977e349
Innovimax team <kojimch@gmail.com>
2017-03-09 15:08:50 +0100

    move params setting to constructor

--- src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
@@ -18,6 +18,9 @@ import java.util.stream.Collectors;
 */
 public class DefaultLineJoining extends AbstractLineOperation {
 	
+	private final int col1;
+	private final int col2;
+	
 	/**
  	* @param params The list of parameters (maybe empty)
 	* @see innovimax.mixthem.operation.RuleParam
@@ -25,25 +28,18 @@ public class DefaultLineJoining extends AbstractLineOperation {
 	*/
 	public DefaultLineJoining(Map<RuleParam, ParamValue> params) {
 		super(params);
+		this.col1 = this.params.containsKey(RuleParam._JOIN_COL1) ? this.params.get(RuleParam._JOIN_COL1).asInt() : 1;
+		this.col2 = this.params.containsKey(RuleParam._JOIN_COL2) ? this.params.get(RuleParam._JOIN_COL2).asInt() : this.col1;		
 	}	
 
 	@Override
 	public String process(String line1, String line2) throws MixException {
 		String join = null;
 		if (line1 !=null && line2 != null) {
-			int col1 = 1;
-			int col2 = 1;
-			if (this.params.containsKey(RuleParam._JOIN_COL1)) {
-				col1 = this.params.get(RuleParam._JOIN_COL1).asInt();
-				col2 = col1;
-			}
-			if (this.params.containsKey(RuleParam._JOIN_COL2)) {
-				col2 = this.params.get(RuleParam._JOIN_COL2).asInt();			
-			}
 			List<String> list1 = Arrays.asList(line1.split(DEFAULT_SPLIT_CELL_REGEX));
 			List<String> list2 = Arrays.asList(line2.split(DEFAULT_SPLIT_CELL_REGEX));		
-			if (list1.size() >= col1 && list2.size() >= col2 && list1.get(col1 - 1).equals(list2.get(col2 - 1))) {
-				String part1 = list1.get(col1 - 1);
+			if (list1.size() >= this.col1 && list2.size() >= this.col2 && list1.get(this.col1 - 1).equals(list2.get(this.col2 - 1))) {
+				String part1 = list1.get(this.col1 - 1);
 				String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(DEFAULT_CELL_SEPARATOR));
 				String part3 = list2.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(DEFAULT_CELL_SEPARATOR));
 				join = part1 + DEFAULT_CELL_SEPARATOR  + part2 + DEFAULT_CELL_SEPARATOR + part3;				
