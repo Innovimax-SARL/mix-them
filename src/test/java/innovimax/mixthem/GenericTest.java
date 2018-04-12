ea1ba02a0d4557107171069bf0a9bb800b0e522f
Innovimax team <kojimch@gmail.com>
2018-04-12 11:35:32 +0200

    Update GenericTest.java

--- src/test/java/innovimax/mixthem/GenericTest.java
+++ src/test/java/innovimax/mixthem/GenericTest.java
@@ -32,8 +32,8 @@ public class GenericTest {
 		   String prefix = "test" + String.format("%03d", testId) +"_";
 		   URL url1 = getClass().getResource(prefix + "file1.txt");
 		   URL url2 = getClass().getResource(prefix + "file2.txt");
-		   MixThem.LOGGER.fine("--> URL 1 (" + prefix + "file1.txt"+") : " + (url1 != null ? "FOUND" : "NOT FOUND"));
-		   MixThem.LOGGER.fine("--> URL 2 (" + prefix + "file2.txt"+") : " + (url2 != null ? "FOUND" : "NOT FOUND"));
+		   MixThem.LOGGER.info("--> URL 1 (" + prefix + "file1.txt"+") : " + (url1 != null ? "FOUND" : "NOT FOUND"));
+		   MixThem.LOGGER.info("--> URL 2 (" + prefix + "file2.txt"+") : " + (url2 != null ? "FOUND" : "NOT FOUND"));
 		   if( url1 == null || url2 == null) break;
 		   for(Rule rule : Rule.values()) {
 			   MixThem.LOGGER.info("RULE " + rule + " (" + (rule.isImplemented() ? "" : "NOT ") + "IMPLEMENTED)");
@@ -41,7 +41,7 @@ public class GenericTest {
 				   String paramsFile = prefix + "params-" + rule.getExtension() + ".txt";
 				   URL urlP = getClass().getResource(paramsFile);
 				   if (urlP != null) {
-					   MixThem.LOGGER.fine("--> Params (" + paramsFile + ") : FOUND");
+					   MixThem.LOGGER.info("--> Params (" + paramsFile + ") : FOUND");
 				   }
 				   List<RuleRun> runs = RuleRuns.getRuns(rule, urlP);
 				   for (RuleRun run : runs) {
@@ -51,7 +51,7 @@ public class GenericTest {
 					   }
 					   resultFile += ".txt";
 					   URL urlR = getClass().getResource(resultFile);
-					   MixThem.LOGGER.fine("--> Result (" + resultFile + ") : " + (urlR != null ? "FOUND" : "NOT FOUND"));
+					   MixThem.LOGGER.info("--> Result (" + resultFile + ") : " + (urlR != null ? "FOUND" : "NOT FOUND"));
 					   if (urlR != null) {
 						   boolean res = check(new File(url1.getFile()), new File(url2.getFile()), new File(urlR.getFile()), rule, run.getParams());
 						   MixThem.LOGGER.info("RUN " + (res ? "PASS" : "FAIL") + " WITH PARAMS " + run.getParams().toString());
