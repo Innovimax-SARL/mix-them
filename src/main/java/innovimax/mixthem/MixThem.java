7cff5e3d58303307167d83daf25875a8bc4c0c2f
Innovimax team <kojimch@gmail.com>
2018-04-13 12:25:16 +0200

    Update MixThem.java

--- src/main/java/innovimax/mixthem/MixThem.java
+++ src/main/java/innovimax/mixthem/MixThem.java
@@ -1,6 +1,5 @@
 package innovimax.mixthem;
 
-import static innovimax.mixthem.MixConstants.*;
 import innovimax.mixthem.arguments.*;
 import innovimax.mixthem.io.*;
 import innovimax.mixthem.operation.*;
@@ -32,6 +31,7 @@ import java.util.logging.Logger;
 public class MixThem {
     
     public final static Logger LOGGER = Logger.getLogger(MixThem.class.getName());
+    private final static int CHAR_BUFFER_SIZE = 1024;
 
     private final File file1, file2;
     private final OutputStream out;
