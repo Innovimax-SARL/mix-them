12a11c41e3295530d5b5bcc0257bbfd71fcb08f7
Innovimax team <kojimch@gmail.com>
2018-04-13 12:57:33 +0200

    Update AltMode.java

--- src/main/java/innovimax/mixthem/operation/AltMode.java
+++ src/main/java/innovimax/mixthem/operation/AltMode.java
@@ -4,8 +4,8 @@ package innovimax.mixthem.operation;
 * <p>This is a enumeration of alternation mode.</p>
 * <p>Here are the modes:</p>
 * <ul>
-* <li>_NORMAL: alternate files each turn</li>
-* <li>_RANDOM: alternate files randomly</li>
+* <li>NORMAL: alternate files each turn</li>
+* <li>RANDOM: alternate files randomly</li>
 * </ul>
 * @author Innovimax
 * @version 1.0
