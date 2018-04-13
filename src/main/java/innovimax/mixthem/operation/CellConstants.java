60ba7a8910ccb27b71de681bad983f7e3c72dec8
Innovimax team <kojimch@gmail.com>
2018-04-13 11:25:21 +0200

    Create CellConstants.java

---
+++ src/main/java/innovimax/mixthem/operation/CellConstants.java
@@ -0,0 +1,6 @@
+package innovimax.mixthem.operation;
+
+public final class CellConstants {
+  public final static String DEFAULT_SPLIT_CELL_REGEX = "\\s";
+  public final static String DEFAULT_CELL_SEPARATOR = " ";
+}
