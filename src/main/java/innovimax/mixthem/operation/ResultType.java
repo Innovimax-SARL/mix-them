2a34f2be68924918bbe06a9ecbde4ca9ff83b375
Innovimax team <kojimch@gmail.com>
2018-04-13 16:06:25 +0200

    Update ResultType.java

--- src/main/java/innovimax/mixthem/operation/ResultType.java
+++ src/main/java/innovimax/mixthem/operation/ResultType.java
@@ -5,16 +5,14 @@ package innovimax.mixthem.operation;
 * <p>Here are the types:</p>
 * <ul>
 * <li>HAS_RESULT: has a result</li>
-* <li>READ_FIRST_FILE: read first file</li>
-* <li>READ_SECOND_FILE: read second file</li>
-* <li>READ_BOTH_FILES: read both files</li>
+* <li>PRESERVE_FIRST_LINE: don't read first file</li>
+* <li>PRESERVE_SECOND_LINE: don't read second file</li>
 * </ul>
 * @author Innovimax
 * @version 1.0
 */
 public enum ResultType {
     HAS_RESULT,
-    READ_FIRST_FILE,
-    READ_SECOND_FILE,
-    READ_BOTH_FILES;
+    PRESERVE_FIRST_LINE,    
+    PRESERVE_SECOND_LINE;
 }
