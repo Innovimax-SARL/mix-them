9681867a141832de44396868617808231abe1a0a
Innovimax team <kojimch@gmail.com>
2018-04-13 12:56:38 +0200

    Update ResultType.java

--- src/main/java/innovimax/mixthem/operation/ResultType.java
+++ src/main/java/innovimax/mixthem/operation/ResultType.java
@@ -4,19 +4,19 @@ package innovimax.mixthem.operation;
 * <p>This is a enumeration of result type.</p>
 * <p>Here are the types:</p>
 * <ul>
-* <li>_GET_RESULT: get the result</li>
-* <li>_IGNORE_RESULT: ignore the result</li>
-* <li>_READ_FIRST_FILE: read first file</li>
-* <li>_READ_SECOND_FILE: read second file</li>
-* <li>_READ_BOTH_FILES: read both files</li>
+* <li>GET_RESULT: get the result</li>
+* <li>IGNORE_RESULT: ignore the result</li>
+* <li>READ_FIRST_FILE: read first file</li>
+* <li>READ_SECOND_FILE: read second file</li>
+* <li>READ_BOTH_FILES: read both files</li>
 * </ul>
 * @author Innovimax
 * @version 1.0
 */
 public enum ResultType {
-    _GET_RESULT,
-    _IGNORE_RESULT,
-    _READ_FIRST_FILE,
-    _READ_SECOND_FILE,
-    _READ_BOTH_FILES;
+    GET_RESULT,
+    IGNORE_RESULT,
+    READ_FIRST_FILE,
+    READ_SECOND_FILE,
+    READ_BOTH_FILES;
 }
