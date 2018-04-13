62f3668debb4726dab93c26fe82d3306514e478f
Innovimax team <kojimch@gmail.com>
2018-04-13 11:33:27 +0200

    Update and rename JoinConstants.java to JoinOperation.java

--- src/main/java/innovimax/mixthem/operation/JoinConstants.java
+++
@@ -1,5 +0,0 @@
-package innovimax.mixthem.operation;
-
-public final class JoinConstants {  
-  public final static int DEFAULT_JOIN_COLUMN = 1;
-}
---
+++ src/main/java/innovimax/mixthem/operation/JoinOperation.java
@@ -0,0 +1,5 @@
+package innovimax.mixthem.operation;
+
+interface JoinOperation {  
+  final static int DEFAULT_JOIN_COLUMN = 1;
+}
