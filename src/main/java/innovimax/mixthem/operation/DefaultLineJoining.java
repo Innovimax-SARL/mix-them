e1238bc4c3211166c75b083d4f458545172cace7
Innovimax team <kojimch@gmail.com>
2018-04-13 12:17:02 +0200

    Update DefaultLineJoining.java

--- src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
@@ -36,13 +36,14 @@ public class DefaultLineJoining extends AbstractLineOperation {
 	public void process(String line1, String line2, LineResult result) throws MixException {
 		result.resetTypes();
 		if (line1 !=null && line2 != null) {
-			List<String> list1 = Arrays.asList(line1.split(DEFAULT_SPLIT_CELL_REGEX));
-			List<String> list2 = Arrays.asList(line2.split(DEFAULT_SPLIT_CELL_REGEX));		
+			List<String> list1 = Arrays.asList(line1.split(CellOperation.DEFAULT_SPLIT_CELL_REGEX.toString()));
+			List<String> list2 = Arrays.asList(line2.split(CellOperation.DEFAULT_SPLIT_CELL_REGEX.toString()));		
 			if (list1.size() >= this.col1 && list2.size() >= this.col2 && list1.get(this.col1 - 1).equals(list2.get(this.col2 - 1))) {
 				String part1 = list1.get(this.col1 - 1);
-				String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(DEFAULT_CELL_SEPARATOR));
-				String part3 = list2.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(DEFAULT_CELL_SEPARATOR));
-				result.setResult(part1 + DEFAULT_CELL_SEPARATOR  + part2 + DEFAULT_CELL_SEPARATOR + part3);
+				String part2 = list1.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(CellOperation.DEFAULT_CELL_SEPARATOR.toString()));
+				String part3 = list2.stream().filter(s -> !s.equals(part1)).collect(Collectors.joining(CellOperation.DEFAULT_CELL_SEPARATOR.toString()));
+				result.setResult(part1 + CellOperation.DEFAULT_CELL_SEPARATOR.toString()  + 
+						 part2 + CellOperation.DEFAULT_CELL_SEPARATOR.toString() + part3);
 			}
 		} else {
 			result.ignoreResult();
