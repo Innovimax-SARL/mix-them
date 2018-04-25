fb057f5e8bd8871fc639943875eb2cf24bccdd18
Innovimax team <kojimch@gmail.com>
2018-04-25 10:26:16 +0200

    Update DefaultLineReader.java

--- src/main/java/innovimax/mixthem/io/DefaultLineReader.java
+++ src/main/java/innovimax/mixthem/io/DefaultLineReader.java
@@ -2,6 +2,8 @@ package innovimax.mixthem.io;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -16,17 +18,24 @@ import java.nio.file.Path;
 */
 public class DefaultLineReader implements IInputLine {
 
-	private final Path path;
 	private final BufferedReader reader;
 
 	/**
- 	* Creates a line reader.
+ 	* Creates a line reader from a file.
  	* @param input The input file to be read 	
  	* @throws IOException - If an I/O error occurs
  	*/
 	public DefaultLineReader(File input) throws IOException {
-		this.path = input.toPath();
-		this.reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);		
+		this.reader = Files.newBufferedReader(input.toPath(), StandardCharsets.UTF_8);		
+	}
+	
+	/**
+ 	* Creates a line reader from an input stream.
+ 	* @param input The input stream to be read 	
+ 	* @throws IOException - If an I/O error occurs
+ 	*/
+	public DefaultLineReader(InputStream input) throws IOException {
+	        this.reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
 	}
 	
 	@Override
