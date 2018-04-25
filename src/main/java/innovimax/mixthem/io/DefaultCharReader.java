608710d1ce479037501b6383bfa12ce224e779d3
Innovimax team <kojimch@gmail.com>
2018-04-25 15:02:07 +0200

    Update DefaultCharReader.java

--- src/main/java/innovimax/mixthem/io/DefaultCharReader.java
+++ src/main/java/innovimax/mixthem/io/DefaultCharReader.java
@@ -1,12 +1,7 @@
 package innovimax.mixthem.io;
 
-import java.io.File;
 import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 /**
 * <p>Reads characters from a character-input file.</p>
