c5f39fbbba8949f2f9dee6406f44e7f6d7d09e85
Innovimax team <kojimch@gmail.com>
2018-04-25 11:54:39 +0200

    Update DefaultCharReader.java

--- src/main/java/innovimax/mixthem/io/DefaultCharReader.java
+++ src/main/java/innovimax/mixthem/io/DefaultCharReader.java
@@ -2,6 +2,8 @@ package innovimax.mixthem.io;
 
 import java.io.File;
 import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -16,17 +18,24 @@ import java.nio.file.Path;
 */
 public class DefaultCharReader implements IInputChar {
 
-	private final Path path;
 	private final BufferedReader reader;
 
 	/**
- 	* Creates a character reader.
+ 	* Creates a character reader from a file.
  	* @param input The input file to be read
  	* @throws IOException - If an I/O error occurs
  	*/
 	public DefaultCharReader(File input) throws IOException {		
-		this.path = input.toPath();
 		this.reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+	}
+	
+	/**
+ 	* Creates a character reader from an input stream.
+ 	* @param input The input stream to be read
+ 	* @throws IOException - If an I/O error occurs
+ 	*/
+	public DefaultCharReader(InputStream input) throws IOException {
+		this.reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
 	}
 
 	@Override
