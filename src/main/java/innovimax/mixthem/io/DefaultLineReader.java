b693a97e7f301459327302d41299b5b69fa1d8e7
Innovimax team <kojimch@gmail.com>
2018-04-25 14:50:50 +0200

    Update DefaultLineReader.java

--- src/main/java/innovimax/mixthem/io/DefaultLineReader.java
+++ src/main/java/innovimax/mixthem/io/DefaultLineReader.java
@@ -1,12 +1,7 @@
 package innovimax.mixthem.io;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 /**
 * <p>Reads lines from a character-input file.</p>
