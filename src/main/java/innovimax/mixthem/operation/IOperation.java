28ae6cfa38b8669f1ed02fb7f81fe0d1ca6e70f0
Innovimax team <kojimch@gmail.com>
2018-04-25 10:40:37 +0200

    Update IOperation.java

--- src/main/java/innovimax/mixthem/operation/IOperation.java
+++ src/main/java/innovimax/mixthem/operation/IOperation.java
@@ -3,9 +3,11 @@ package innovimax.mixthem.operation;
 import innovimax.mixthem.MixException;
 
 import java.io.File;
+import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public interface IOperation {
   void processFiles(File file1, File file2, OutputStream out) throws MixException, IOException;
+  void processFiles(InputStream input1, InputStream input2, OutputStream out) throws MixException, IOException;
 }
