9624dac364ed7eabb7d23261cf8e55ba2d1b5742
Innovimax team <kojimch@gmail.com>
2018-04-26 11:07:28 +0200

    Update BasicTest.java

--- src/test/java/innovimax/mixthem/BasicTest.java
+++ src/test/java/innovimax/mixthem/BasicTest.java
@@ -21,31 +21,31 @@ public class BasicTest {
     }
     
     @Test(expected=ArgumentException.class)
-    public final void testEmptyArgs() throws ArgumentException, IOException, ZIPException {
+    public final void testEmptyArgs() throws ArgumentException, IOException, ZipException {
         final String args[] = {};
         Arguments mixArgs = Arguments.checkArguments(args);
     }
 
     @Test(expected=ArgumentException.class)
-    public final void testWrongArgs() throws ArgumentException, IOException, ZIPException {
+    public final void testWrongArgs() throws ArgumentException, IOException, ZipException {
         final String args[] = { "ghost1", "ghost2" };
         Arguments mixArgs = Arguments.checkArguments(args);
     }
 
     @Test
-    public final void testNoRule() throws ArgumentException, IOException, ZIPException {
+    public final void testNoRule() throws ArgumentException, IOException, ZipException {
         final String args[] = { getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
         Assert.assertTrue(true);
     }
     @Test
-    public final void test1Rule() throws ArgumentException, IOException, ZIPException {
+    public final void test1Rule() throws ArgumentException, IOException, ZipException {
         final String args[] = { "-1", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
         Assert.assertTrue(true);
     }
     @Test(expected=ArgumentException.class)
-    public final void testUnknownRule() throws ArgumentException, IOException, ZIPException {
+    public final void testUnknownRule() throws ArgumentException, IOException, ZipException {
         final String args[] = { "-x", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
     }
@@ -57,26 +57,26 @@ public class BasicTest {
     }
 
     @Test(expected=ArgumentException.class)
-    public final void testWrongSeedParam() throws ArgumentException, IOException, ZIPException {
+    public final void testWrongSeedParam() throws ArgumentException, IOException, ZipException {
         final String args[] = { "-random-alt-line", "#val", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
     }
 
     @Test
-    public final void testValidSeedParam() throws ArgumentException, IOException, ZIPException {
+    public final void testValidSeedParam() throws ArgumentException, IOException, ZipException {
         final String args[] = { "-random-alt-line", "#1789", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
         Assert.assertTrue(true);        
     }
     
     @Test(expected=ArgumentException.class)
-    public final void testNotAParam() throws ArgumentException, IOException, ZIPException {
+    public final void testNotAParam() throws ArgumentException, IOException, ZipException {
         final String args[] = { "-random-alt-line", "1789", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
     }
 
     @Test
-    public final void testOptionalParam() throws ArgumentException, IOException, ZIPException {
+    public final void testOptionalParam() throws ArgumentException, IOException, ZipException {
         final String args[] = { "-random-alt-line", getClass().getResource("test001_file1.txt").getFile(), getClass().getResource("test001_file2.txt").getFile() };
         Arguments mixArgs = Arguments.checkArguments(args);
         Assert.assertTrue(true);        
