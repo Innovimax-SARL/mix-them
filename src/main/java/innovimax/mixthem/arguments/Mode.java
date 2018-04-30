9f00bcb076a8cc17e22dcac95e3d3af115f091c1
Innovimax team <kojimch@gmail.com>
2018-04-30 17:05:29 +0200

    Update Mode.java

--- src/main/java/innovimax/mixthem/arguments/Mode.java
+++ src/main/java/innovimax/mixthem/arguments/Mode.java
@@ -1,6 +1,25 @@
 package innovimax.mixthem.operation;
 
+/**
+* <p>This is a detailed enumeration of modes used to mix files.</p>
+* @author Innovimax
+* @version 1.0
+*/
 public enum Mode {	
-	CHAR,
-	BYTE;
+	CHAR("char"),
+	BYTE("byte");
+	
+	private final String name;
+
+    private Mode(final String name) {
+        this.name = name;
+    }
+	
+	/**
+    * Returns the name of this mode on command line.
+    * @return The name of the mode on command line
+    */
+    public String getName() {
+        return this.name;
+    }
 }
