2305a665277967b7fb4db81b173358e5947f9165
Innovimax team <kojimch@gmail.com>
2018-05-02 15:56:25 +0200

    Create IOutputByte.java

---
+++ src/main/java/innovimax/mixthem/io/IOutputByte.java
@@ -0,0 +1,29 @@
+package innovimax.mixthem.io;
+
+import java.io.IOException;
+
+/**
+* This interface provides for writing bytes in an output stream.
+* @author Innovimax
+* @version 1.0
+*/
+public interface IOutputByte {
+	/**
+ 	* Writes a single byte.
+ 	* @param b The byte as an int to be written
+ 	* @throws IOException - If an I/O error occurs
+ 	*/	
+	void writeByte(int b) throws IOException;
+	/**
+ 	* Writes a portion of an array of bytes.
+ 	* @param buffer Buffer of bytes
+ 	* @param len Number of bytes to write
+ 	* @throws IOException - If an I/O error occurs
+ 	*/	
+	void writeCharacters(byte[] buffer, int len) throws IOException;	
+	/**
+	* Closes this output and releases any system resources associated with it.
+ 	* @throws IOException - If an I/O error occurs	
+	*/
+	void close() throws IOException;		
+}
