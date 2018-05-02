2c2206359e9fbd693e6736bf434bdab1b9d97a56
Innovimax team <kojimch@gmail.com>
2018-05-02 14:37:10 +0200

    Create CharCopy.java

---
+++ src/main/java/innovimax/mixthem/operation/CharCopy.java
@@ -0,0 +1,33 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.io.DefaultCharReader;
+import innovimax.mixthem.io.DefaultCharWriter;
+import innovimax.mixthem.io.IInputChar;
+import innovimax.mixthem.io.IOutputChar;
+import innovimax.mixthem.io.InputResource;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+* <p>Copy all characters.</p>
+* @author Innovimax
+* @version 1.0
+*/
+public class CharCopy implements ICopy {
+
+    private final static int CHAR_BUFFER_SIZE = 1024;
+    
+    public void processFile(InputResource input, OutputStream out) throws IOException    
+        char[] buffer = new char[CHAR_BUFFER_SIZE];
+        IInputChar reader = new DefaultCharReader(input);
+        IOutputChar writer = new DefaultCharWriter(out);
+        while (reader.hasCharacter()) {
+            final int len = reader.nextCharacters(buffer, CHAR_BUFFER_SIZE);
+            writer.writeCharacters(buffer, len);
+        }
+        reader.close();
+        writer.close();
+    }    
+    
+}
