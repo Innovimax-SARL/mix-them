59ecad6cdcd06837d4d51200c46decd803188e42
Innovimax team <kojimch@gmail.com>
2018-05-03 15:16:15 +0200

    Update DefaultByteAlternation.java

--- src/main/java/innovimax/mixthem/operation/DefaultByteAlternation.java
+++ src/main/java/innovimax/mixthem/operation/DefaultByteAlternation.java
@@ -6,6 +6,7 @@ import innovimax.mixthem.arguments.ParamValue;
 
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Random;
 
 /**
 * <p>Alternate two bytes.</p>
@@ -15,17 +16,22 @@ import java.util.Map;
 */
 public class DefaultByteAlternation extends AbstractByteOperation {
 	
+	private final AltMode mode;
 	private boolean odd;
+	private final Random random;
 	
 	/**
 	* Constructor
+	* @param mode The alternate mode to process
  	* @param params The list of parameters (maybe empty)
 	* @see innovimax.mixthem.arguments.RuleParam
 	* @see innovimax.mixthem.arguments.ParamValue
 	*/
-	public DefaultByteAlternation(final Map<RuleParam, ParamValue> params) {
+	public DefaultByteAlternation(final AltMode mode, final Map<RuleParam, ParamValue> params) {
 		super(params);
+		this.mode = mode;
 		this.odd = true;
+		this.random = new Random(params.getOrDefault(RuleParam.RANDOM_SEED, AltOperation.DEFAULT_RANDOM_SEED.getValue()).asInt());
 	}
 	
 	@Override
@@ -36,9 +42,16 @@ public class DefaultByteAlternation extends AbstractByteOperation {
 			array[0] = b2;
 		} else if (b2 == -1) {
 			array[0] = b1;
-		} else {					
-			array[0] = this.odd ? b1 : b2;
-			this.odd = !this.odd;			
+		} else {
+			switch (this.mode) {
+				case RANDOM:					
+					array[0] = this.random.nextBoolean() ? b1 : b2;
+					break;
+				case NORMAL:
+				default:
+					array[0] = this.odd ? b1 : b2;
+					this.odd = !this.odd;	
+			}			
 		}
 		result.setResult(Arrays.stream(array));
 	}
