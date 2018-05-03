a39b2c7fcca653c24b4f5bfc7dde2bd6725c6625
Innovimax team <kojimch@gmail.com>
2018-05-03 11:43:21 +0200

    Update AbstractByteOperation.java

--- src/main/java/innovimax/mixthem/operation/AbstractByteOperation.java
+++ src/main/java/innovimax/mixthem/operation/AbstractByteOperation.java
@@ -41,10 +41,11 @@ public abstract class AbstractByteOperation extends AbstractOperation implements
 			final int b1 = reader1.nextByte();
 			final int b2 = reader2.nextByte();			
 			process(b1, b2, result);
-			System.out.println("B1="+b1+" B2="+b2+" RES="+result.getResult().toArray());
+			System.out.println("B1="+b1+" B2="+b2+" RES="+result.hasResult());
 			if (result.hasResult()) {
 				result.getResult().forEach(b -> {
 					try {
+						System.out.println("BW="+b);
 						writer.writeByte(b);
 					} catch (IOException e) {
 						throw new RuntimeException(e);
