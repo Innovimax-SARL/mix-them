50f2e2178d4bebe3a54372d438a7843551f82291
Innovimax team <kojimch@gmail.com>
2018-05-07 09:13:14 +0200

    Update and rename IInputByte.java to IByteInput.java

--- src/main/java/innovimax/mixthem/io/IByteInput.java
+++ src/main/java/innovimax/mixthem/io/IByteInput.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 * @author Innovimax
 * @version 1.0
 */
-public interface IInputByte {
+public interface IByteInput {
     /**
 	* Returns true if there is more bytes.
 	* @return Returns true if there is more bytes
