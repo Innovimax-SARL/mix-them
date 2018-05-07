afcf318198fbfcac6a29efac1f07c6100f559b0d
Innovimax team <kojimch@gmail.com>
2018-05-07 15:18:41 +0200

    Update DefaultByteAlternation.java

--- src/main/java/innovimax/mixthem/operation/DefaultByteAlternation.java
+++ src/main/java/innovimax/mixthem/operation/DefaultByteAlternation.java
@@ -37,58 +37,9 @@ public class DefaultByteAlternation extends AbstractByteOperation {
 	}
 	
 	@Override
-	public void process(final byte b1, final byte b2, final ByteResult result) throws MixException {
-		result.reset();
-		final int[] array = new int[1];
-		if (b1 == -1) {
-			array[0] = b2;
-		} else if (b2 == -1) {
-			array[0] = b1;
-		} else {
-			switch (this.mode) {
-				case RANDOM:					
-					array[0] = this.random.nextBoolean() ? b1 : b2;
-					break;
-				case NORMAL:
-				default:
-					array[0] = this.odd ? b1 : b2;
-					this.odd = !this.odd;	
-			}			
-		}
-		result.setResult(Arrays.stream(array));
-	}
-	
-	@Override
 	public void process(byte[] bytes, ByteResult result) throws MixException {
 		result.reset();
 		final int[] array = new int[1];
-		/*
-		switch (this.mode) {
-			case RANDOM:				
-				int channel = this.random.nextInt(bytes.length);
-				byte br = bytes[channel];
-				System.out.println("BYTES+"+Arrays.toString(bytes)+" CHANNEL="+channel+" BYTE="+br);
-				if (br == -1) {					
-					channel = nextChannel(bytes, channel);
-					br = bytes[channel];
-					System.out.println("NEW_CHANNEL="+channel+" BYTE="+br);
-				}
-				array[0] = br;
-				break;
-			case NORMAL:
-			default:				
-				byte bn = bytes[this.channel];
-				if (bn == -1) {
-					this.channel = nextChannel(bytes, this.channel);
-					bn = bytes[this.channel];
-				}
-				array[0] = bn;
-				this.channel++;	
-				if (this.channel == bytes.length) {
-					this.channel = 0;
-				}
-		}
-		*/
 		int channel = this.mode == AltMode.NORMAL ? this.channel : this.random.nextInt(bytes.length);
 		byte b = bytes[channel];
 		System.out.println("BYTES+"+Arrays.toString(bytes)+" CHANNEL="+channel+" BYTE="+b);
