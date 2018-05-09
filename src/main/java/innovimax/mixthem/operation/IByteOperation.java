a7aef023908fcef974420b84a88fd6f6bec79f77
Innovimax team <kojimch@gmail.com>
2018-05-09 10:44:38 +0200

    Update IByteOperation.java

--- src/main/java/innovimax/mixthem/operation/IByteOperation.java
+++ src/main/java/innovimax/mixthem/operation/IByteOperation.java
@@ -6,11 +6,11 @@ import innovimax.mixthem.MixException;
 interface IByteOperation extends IOperation {
 	/**
  	* Processes operation and set new result in the ByteResult parameter.
-	* @param bytes The array of bytes to mix	
+	* @param bytes The range of bytes to mix
 	* @param result The previous operation result
  	* @return The result of the operation (maybe null)
  	* @throws MixException - If an mixing error occurs
 	* @see innovimax.mixthem.operation.ByteResult	
 	*/	
-	void process(byte[] bytes, ByteResult result) throws MixException;
+	void process(byte[] byteRange, ByteResult result) throws MixException;
 }
