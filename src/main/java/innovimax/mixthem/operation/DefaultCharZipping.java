c883c6fd9a05afbbd51229efd207f01231ffa401
Innovimax team <kojimch@gmail.com>
2018-05-09 14:35:22 +0200

    Update DefaultCharZipping.java

--- src/main/java/innovimax/mixthem/operation/DefaultCharZipping.java
+++ src/main/java/innovimax/mixthem/operation/DefaultCharZipping.java
@@ -60,7 +60,7 @@ public class DefaultCharZipping extends AbstractCharOperation {
 		for (int i=0; i < charRange.length; i++) {			
 			if (charRange[i] != -1) {
 				array[index++] = charRange[i];
-			} else {
+			} else if (index < array.length) {
         			for (int j = 0; j < sep.length(); j++) {
 					array[index++] = (int) sep.charAt(j);
 				}				
