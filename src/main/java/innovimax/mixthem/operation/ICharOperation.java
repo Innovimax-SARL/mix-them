9b342beeb4272899acefe54c05315e25a87b8ee2
Innovimax team <kojimch@gmail.com>
2018-05-11 11:48:09 +0200

    Update ICharOperation.java

--- src/main/java/innovimax/mixthem/operation/ICharOperation.java
+++ src/main/java/innovimax/mixthem/operation/ICharOperation.java
@@ -6,16 +6,6 @@ import innovimax.mixthem.MixException;
 interface ICharOperation extends IOperation {
 	/**
  	* Processes operation and set new result in the CharResult parameter.
-	* @param c1 The first character to mix as an int (maybe -1)
-	* @param c2 The second character to mix as an int (maybe -1)
-	* @param result The previous operation result
- 	* @return The result of the operation (maybe null)
- 	* @throws MixException - If an mixing error occurs
-	* @see innovimax.mixthem.operation.CharResult	
-	*/	
-	void process(int c1, int c2, CharResult result) throws MixException;
-	/**
- 	* Processes operation and set new result in the CharResult parameter.
 	* @param characterRange The range of characters as int to mix	
 	* @param result The previous operation result
  	* @return The result of the operation (maybe null)
