ef0a807e5e9e290406429707bc1951a2a60a5480
Innovimax team <kojimch@gmail.com>
2018-05-11 11:46:43 +0200

    Update DefaultCharZipping.java

--- src/main/java/innovimax/mixthem/operation/DefaultCharZipping.java
+++ src/main/java/innovimax/mixthem/operation/DefaultCharZipping.java
@@ -48,7 +48,7 @@ public class DefaultCharZipping extends AbstractCharOperation {
 	@Override
 	public void process(final int[] charRange, final CharResult result) throws MixException {
 		result.reset();
-		System.out.println("RANGE="+Arrays.toString(charRange));
+		//System.out.println("RANGE="+Arrays.toString(charRange));
 		boolean zipable = true;		
 		for (int i=0; i < charRange.length; i++) {			
 			if (charRange[i] == -1) {
