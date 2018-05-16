177de4b969e8aa370406320726ca02b6ba8dcc21
Innovimax team <kojimch@gmail.com>
2018-05-16 11:02:35 +0200

    Update DefaultLineJoining.java

--- src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
+++ src/main/java/innovimax/mixthem/operation/DefaultLineJoining.java
@@ -6,7 +6,6 @@ import innovimax.mixthem.arguments.RuleParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
