5f812f649b03e5d2653d064bb9adfc8019fa3558
Innovimax team <kojimch@gmail.com>
2018-05-16 09:45:16 +0200

    Update DefaultCellZipping.java

--- src/main/java/innovimax/mixthem/operation/DefaultCellZipping.java
+++ src/main/java/innovimax/mixthem/operation/DefaultCellZipping.java
@@ -6,7 +6,6 @@ import innovimax.mixthem.arguments.ParamValue;
 
 import java.util.Arrays;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
