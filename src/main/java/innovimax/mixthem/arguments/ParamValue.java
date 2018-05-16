cd2896b520249a7c5c859411020702231e77c1c2
Innovimax team <kojimch@gmail.com>
2018-05-16 15:47:57 +0200

    Update ParamValue.java

--- src/main/java/innovimax/mixthem/arguments/ParamValue.java
+++ src/main/java/innovimax/mixthem/arguments/ParamValue.java
@@ -7,12 +7,12 @@ package innovimax.mixthem.arguments;
 * @version 1.0
 */
 public abstract class ParamValue {
+	
 	private static class ParamStringValue extends ParamValue {
 		private final String text;
 		private ParamStringValue(final String text) {
 			this.text = text;
 		}
-		
 		@Override
 		public String asString() {
 			return this.text;
@@ -22,29 +22,59 @@ public abstract class ParamValue {
 			throw new UnsupportedOperationException("ParamStringValue does not have an int representation");
 		}
 		@Override
+		public int[] asIntArray() {
+			throw new UnsupportedOperationException("ParamStringValue does not have an int[] representation");
+		}
+		
+		@Override
 		public String toString() {
 			return this.text;
 		}
 	}
+	
 	private static class ParamIntValue extends ParamValue {
 		private final int i;
 		private ParamIntValue(final int i) {
 			this.i = i;
 		}			
-		
 		@Override
 		public String asString() {
 			throw new UnsupportedOperationException("ParamIntValue does not have a String representation");
 		}
-
 		@Override
 		public int asInt() {
 			return this.i;
 		}
-		
+		@Override
+		public int[] asIntArray() {
+			throw new UnsupportedOperationException("ParamIntValue does not have an int[] representation");
+		}
 		@Override
 		public String toString() {
 			return Integer.toString(i);
+		}
+	}
+	
+	private static class ParamIntArrayValue extends ParamValue {
+		private final int[] array;
+		private ParamIntArrayValue(final int[] array) {
+			this.array = array;
+		}			
+		@Override
+		public String asString() {
+			throw new UnsupportedOperationException("ParamIntArrayValue does not have a String representation");
+		}
+		@Override
+		public int asInt() {
+			throw new UnsupportedOperationException("ParamIntArrayValue does not have an int representation");
+		}
+		@Override
+		public int[] asIntArray() {
+			return this.array;
+		}
+		@Override
+		public String toString() {
+			return Arrays.toString(array);
 		}
 	}
 	
@@ -59,6 +89,10 @@ public abstract class ParamValue {
 	
 	public static ParamValue createInt(final int i) {
 		return new ParamIntValue(i);
+	}
+	
+	public static ParamValue createIntArray(final int[] array) {
+		return new ParamIntArrayValue(array);
 	}
 	
 	/**
