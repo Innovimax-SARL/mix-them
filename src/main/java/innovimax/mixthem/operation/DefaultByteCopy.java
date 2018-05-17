657e2adbfe8326eb0a1c9eed08f98da692425fca
Innovimax team <kojimch@gmail.com>
2018-05-17 16:46:01 +0200

    Update DefaultByteCopy.java

--- src/main/java/innovimax/mixthem/operation/DefaultByteCopy.java
+++ src/main/java/innovimax/mixthem/operation/DefaultByteCopy.java
@@ -16,29 +16,28 @@ import java.io.OutputStream;
 */
 public class DefaultByteCopy extends AbstractCopy {
 
-    private final static int BYTE_BUFFER_SIZE = 1024;
+	private final static int BYTE_BUFFER_SIZE = 1024;
     
-    /**
-	* Constructor	
- 	* @param params The list of parameters (maybe empty)
+	/**
+	* Constructor
+	* @param params The list of parameters (maybe empty)
 	* @see innovimax.mixthem.arguments.RuleParam
 	* @see innovimax.mixthem.arguments.ParamValue
 	*/
 	public DefaultByteCopy(final Map<RuleParam, ParamValue> params) {
-		super(params);		
+		super(params);
 	}
-    
-    @Override
-    public void processFile(InputResource input, OutputStream out) throws IOException {
-        byte[] buffer = new byte[BYTE_BUFFER_SIZE];
-        IByteInput reader = new DefaultByteReader(input);
-        IByteOutput writer = new DefaultByteWriter(out);
-        while (reader.hasByte()) {
-            final int len = reader.nextBytes(buffer, BYTE_BUFFER_SIZE);
-            writer.writeBytes(buffer, len);
-        }
-        reader.close();
-        writer.close();
-    }    
-    
+	@Override
+	public void processFile(InputResource input, OutputStream out) throws IOException {
+		byte[] buffer = new byte[BYTE_BUFFER_SIZE];
+		IByteInput reader = new DefaultByteReader(input);
+		IByteOutput writer = new DefaultByteWriter(out);
+		while (reader.hasByte()) {
+			final int len = reader.nextBytes(buffer, BYTE_BUFFER_SIZE);
+			writer.writeBytes(buffer, len);
+		}
+		reader.close();
+		writer.close();
+	}    
+
 }
