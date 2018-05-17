bd5d831ddb47f5925073e10032d5c8cee2da1274
Innovimax team <kojimch@gmail.com>
2018-05-17 16:40:27 +0200

    Create AbstractCopy.java

---
+++ src/main/java/innovimax/mixthem/operation/AbstractCopy.java
@@ -0,0 +1,24 @@
+package innovimax.mixthem.operation;
+
+import innovimax.mixthem.arguments.RuleParam;
+import innovimax.mixthem.arguments.ParamValue;
+
+/**
+* <p>Abstract class for all copy operation.</p>
+* @see ICopy
+* @author Innovimax
+* @version 1.0
+*/
+public abstract class AbstractCopy extends AbstractOperation implements ICopy {
+
+	/**
+	* Constructor
+ 	* @param params The list of parameters (maybe empty)
+	* @see innovimax.mixthem.arguments.RuleParam
+	* @see innovimax.mixthem.arguments.ParamValue
+	*/
+	public AbstractCopy(final Map<RuleParam, ParamValue> params) {
+		super(params);
+	}
+
+}
