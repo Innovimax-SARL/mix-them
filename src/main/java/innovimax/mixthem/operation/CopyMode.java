f811ecad7f5f7b0aa25dcf03ba04d6f8ea8b4427
Innovimax team <kojimch@gmail.com>
2018-05-18 12:38:38 +0200

    Create CopyMode.java

---
+++ src/main/java/innovimax/mixthem/operation/CopyMode.java
@@ -0,0 +1,18 @@
+package innovimax.mixthem.operation;
+
+/**
+* <p>This is a enumeration of copy mode.</p>
+* <p>Here are the modes:</p>
+* <ul>
+* <li>FIRST: copy first file</li>
+* <li>SECOND: copy second file</li>
+* <li>ALL: copy all files</li>
+* </ul>
+* @author Innovimax
+* @version 1.0
+*/
+public enum CopyMode {	
+	FIRST,
+	SECOND,
+	ALL;
+}
