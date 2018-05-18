ccfada646e11e178362c0502a888cd6113f6beff
Innovimax team <kojimch@gmail.com>
2018-05-18 12:52:49 +0200

    Update MixThem.java

--- src/main/java/innovimax/mixthem/MixThem.java
+++ src/main/java/innovimax/mixthem/MixThem.java
@@ -98,16 +98,16 @@ public class MixThem {
             LOGGER.info("Started mixing for [" +  mode.getName() + "] rule '" + rule.getName() + "'...");
             switch(rule) {
                 case FILE_1:
-                    final IOperation copyOp = CopyFactory.newInstance(mode, CopyMode.FIRST, params);
-                    copyOp.processFiles(this.inputs, this.output);                    
+                    final IOperation copyFile1Op = CopyFactory.newInstance(mode, CopyMode.FIRST, params);
+                    copyFile1Op.processFiles(this.inputs, this.output);                    
                     break;
                 case FILE_2:
-                    final IOperation copyOp = CopyFactory.newInstance(mode, CopyMode.SECOND, params);
-                    copyOp.processFiles(this.inputs, this.output);          
+                    final IOperation copyFile2Op = CopyFactory.newInstance(mode, CopyMode.SECOND, params);
+                    copyFile2Op.processFiles(this.inputs, this.output);          
                     break;
                 case ADD:                    
-                    final IOperation copyOp = CopyFactory.newInstance(mode, CopyMode.ALL, params);
-                    copyOp.processFiles(this.inputs, this.output);          
+                    final IOperation copyAddOp = CopyFactory.newInstance(mode, CopyMode.ALL, params);
+                    copyAddOp.processFiles(this.inputs, this.output);          
                     break;
                 case ALT_CHAR:
                     final IOperation altCharOp = new DefaultCharAlternation(AltMode.NORMAL, params);
