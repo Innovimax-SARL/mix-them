8bb274f103a966ac9dd7dcffc6729d4550367879
Innovimax team <kojimch@gmail.com>
2018-05-18 09:51:18 +0200

    Update ICopyOperation.java

--- src/main/java/innovimax/mixthem/operation/ICopyOperation.java
+++ src/main/java/innovimax/mixthem/operation/ICopyOperation.java
@@ -5,6 +5,12 @@ import innovimax.mixthem.io.InputResource;
 import java.io.IOException;
 import java.io.OutputStream;
 
-public interface ICopyOperation {
-    void process(InputResource input, OutputStream out) throws IOException;
+public interface ICopyOperation extends IOperation {
+    /**
+ 	* Processes file copy.
+	* @param input Input resource to be copied
+	* @param output Output result writer 	
+ 	* @throws IOException - If an i/o error occurs	
+	*/	
+    void process(InputResource input, OutputStream output) throws IOException;
 }
