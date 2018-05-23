8517b537df5ab8836653665ff3ed84d53b84846d
Innovimax team <kojimch@gmail.com>
2018-05-23 10:30:24 +0200

    Update Rule.java

--- src/main/java/innovimax/mixthem/arguments/Rule.java
+++ src/main/java/innovimax/mixthem/arguments/Rule.java
@@ -15,7 +15,7 @@ public enum Rule {
     FILE_2("2", "2", "will output file 2", true, 
                 EnumSet.noneOf(RuleParam.class), EnumSet.of(FileMode.CHAR, FileMode.BYTE)),
     FILE_N("file", "file", "will output file n", true, 
-                EnumSet.noneOf(RuleParam.class), EnumSet.of(FileMode.CHAR, FileMode.BYTE)),
+                EnumSet.of(RuleParam.SEL_FILE), EnumSet.of(FileMode.CHAR, FileMode.BYTE)),
     ADD("+", "add", "will output file1+file2+...+fileN", true, 
                 EnumSet.of(RuleParam.ADD_FILES), EnumSet.of(FileMode.CHAR, FileMode.BYTE)),
     ALT_LINE("alt-line", "altline", "will output one line of each starting with first line of file1", true, 
