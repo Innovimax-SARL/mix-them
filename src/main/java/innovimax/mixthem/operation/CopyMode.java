d09b6559b6421311c99d824b8b5fce2d4d66ce40
Innovimax team <kojimch@gmail.com>
2018-05-24 14:35:22 +0200

    Update CopyMode.java

--- src/main/java/innovimax/mixthem/operation/CopyMode.java
+++ src/main/java/innovimax/mixthem/operation/CopyMode.java
@@ -4,8 +4,6 @@ package innovimax.mixthem.operation;
 * <p>This is a enumeration of copy mode.</p>
 * <p>Here are the modes:</p>
 * <ul>
-* <li>FIRST: copy first file</li>
-* <li>SECOND: copy second file</li>* 
 * <li>UMPTEENTH: copy the umpteenth file</li>*
 * <li>SELECTION: copy a selection of files (maybe all by default)</li>
 * </ul>
@@ -13,8 +11,6 @@ package innovimax.mixthem.operation;
 * @version 1.0
 */
 public enum CopyMode {	
-	FIRST,
-	SECOND,
 	UMPTEENTH,
 	SELECTION;
 }
