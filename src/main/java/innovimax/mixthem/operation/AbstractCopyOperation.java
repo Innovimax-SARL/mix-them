aaa088beb48eafacb9022e829b6f834aafe76a75
Innovimax team <kojimch@gmail.com>
2018-05-25 15:15:14 +0200

    Update AbstractCopyOperation.java

--- src/main/java/innovimax/mixthem/operation/AbstractCopyOperation.java
+++ src/main/java/innovimax/mixthem/operation/AbstractCopyOperation.java
@@ -21,43 +21,32 @@ import java.util.stream.IntStream;
 public abstract class AbstractCopyOperation extends AbstractOperation implements ICopyOperation {
 	
 	protected final static int BUFFER_SIZE = 1024;	
-	private final CopyMode copyMode;
 
 	/**
 	* Constructor
-	* @param copyMode The copy mode to process
  	* @param params The list of parameters (maybe empty)
 	* @see innovimax.mixthem.arguments.RuleParam
 	* @see innovimax.mixthem.arguments.ParamValue
 	*/
-	public AbstractCopyOperation(final CopyMode copyMode, final Map<RuleParam, ParamValue> params) {
+	public AbstractCopyOperation(final Map<RuleParam, ParamValue> params) {
 		super(params);
-		this.copyMode = copyMode;
 	}
 
 	@Override
 	public void processFiles(final List<InputResource> inputs, final OutputStream output) throws MixException, IOException {		
-		switch(copyMode) {
-			case UMPTEENTH:
-				int index = params.get(RuleParam.FILE_INDEX).asInt() - 1;
-				process(inputs.get(index), output);
-				break;
-			case SELECTION:
-			default:
-				final IntStream indexes; 
-				if (params.containsKey(RuleParam.FILE_LIST)) {
-					indexes = Arrays.stream(params.get(RuleParam.FILE_LIST).asIntArray());
-				} else {
-					indexes = IntStream.range(1, inputs.size()+1);
-				}				
-                    		indexes.mapToObj(i -> inputs.get(i-1)).forEach(input -> {
-					try {
-			 			process(input, output);
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
-				});				
-		}
+		final IntStream indexes; 
+		if (params.containsKey(RuleParam.FILE_LIST)) {
+			indexes = Arrays.stream(params.get(RuleParam.FILE_LIST).asIntArray());
+		} else {
+			indexes = IntStream.range(1, inputs.size()+1);
+		}				
+                indexes.mapToObj(i -> inputs.get(i-1)).forEach(input -> {
+			try {
+				process(input, output);
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		});
 	}
 
 }
