8e30ee43a1a0a716c3ec371241c1debc307d1d50
Innovimax team <kojimch@gmail.com>
2018-05-25 15:37:24 +0200

    Update CopyFactory.java

--- src/main/java/innovimax/mixthem/operation/CopyFactory.java
+++ src/main/java/innovimax/mixthem/operation/CopyFactory.java
@@ -10,12 +10,11 @@ public class CopyFactory {
   
   	/**
     * @param fileMode The file mode for reading files
-    * @param copyMode the copy mode to process
     * @see innovimax.mixthem.arguments.RulePara
     * @see innovimax.mixthem.arguments.ParamValue
     */
-    public static ICopyOperation newInstance(final FileMode fileMode, final CopyMode copyMode, final Map<RuleParam, ParamValue> params) {
-        return fileMode == FileMode.CHAR ? new DefaultCharCopy(copyMode, params) : new DefaultByteCopy(copyMode, params);
+    public static ICopyOperation newInstance(final FileMode fileMode, final Map<RuleParam, ParamValue> params) {
+        return fileMode == FileMode.CHAR ? new DefaultCharCopy(params) : new DefaultByteCopy(params);
     }    
     
 }
