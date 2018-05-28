94c95255a492760bf0d995d23aaa0622839a2ea6
Innovimax team <kojimch@gmail.com>
2018-05-28 12:45:26 +0200

    Update RuleRuns.java

--- src/test/java/innovimax/mixthem/RuleRuns.java
+++ src/test/java/innovimax/mixthem/RuleRuns.java
@@ -1,8 +1,9 @@
 package innovimax.mixthem;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import innovimax.mixthem.arguments.ParamValue;
 import innovimax.mixthem.arguments.Rule;
