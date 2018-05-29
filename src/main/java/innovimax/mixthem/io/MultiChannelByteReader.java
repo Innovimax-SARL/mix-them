f717a82bf97ec8ff3250d2cbe125ddc452036679
Innovimax team <kojimch@gmail.com>
2018-05-29 11:34:40 +0200

    Update MultiChannelByteReader.java

--- src/main/java/innovimax/mixthem/io/MultiChannelByteReader.java
+++ src/main/java/innovimax/mixthem/io/MultiChannelByteReader.java
@@ -21,7 +21,7 @@ public class MultiChannelByteReader implements IMultiChannelByteInput {
 		IntStream.rangeClosed(1, inputs.size())
 			.filter(index -> selection.isEmpty() || selection.contains(Integer.valueOf(index)))
 			.mapToObj(index -> inputs.get(index-1))
-			.forEach(input -> {
+			.forEachOrdered(input -> {
 				try {
 					this.readers.add(new DefaultByteReader(input));
 				} catch (IOException e) {
