446ef0cf28c0595a018a3dbe343e0ddd50af0bd5
Innovimax team <kojimch@gmail.com>
2018-05-29 15:24:58 +0200

    Update MultiChannelLineReader.java

--- src/main/java/innovimax/mixthem/io/MultiChannelLineReader.java
+++ src/main/java/innovimax/mixthem/io/MultiChannelLineReader.java
@@ -1,8 +1,8 @@
 package innovimax.mixthem.io;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
+//import java.util.ArrayList;
+//import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
