b7634d66d56adda116d273789a8b58915743af2b
Innovimax team <kojimch@gmail.com>
2018-05-31 15:45:01 +0200

    Create token range interface

---
+++ src/main/java/innovimax/mixthem/io/ITokenRange.java
@@ -0,0 +1,25 @@
+package innovimax.mixthem.io;
+
+import java.util.List;
+
+/**
+* This interface provides for reading tokens from a multi-channel input.
+* @author Innovimax
+* @version 1.0
+*/
+public interface ITokenRange {
+    /**
+	* Returns true if there is more tokens in one of the channels.
+	* @return Returns true if there is more tokens in one of the channels
+	*/
+	boolean hasMoreTokens();
+	/**
+ 	* Reads next token range (one by channel, <code>null</code> if no more token in channel)
+ 	* @return The list of tokens (may contains <code>null</code> values)
+ 	*/
+	List<InputToken> nextTokenRange();
+	/**
+	* Closes this input channels and releases any system resources associated with them.
+	*/
+	void close();
+}
