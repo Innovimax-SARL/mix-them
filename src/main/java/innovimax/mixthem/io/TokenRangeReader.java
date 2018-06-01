a7fd29b73e0e1a856a4c586ed018e88bef90e612
Innovimax team <kojimch@gmail.com>
2018-06-01 10:05:00 +0200

    Update TokenRangeReader.java

--- src/main/java/innovimax/mixthem/io/TokenRangeReader.java
+++ src/main/java/innovimax/mixthem/io/TokenRangeReader.java
@@ -1,6 +1,6 @@
 package innovimax.mixthem.io;
 
-import innovimax.mixthem.arguments.Token;
+import innovimax.mixthem.arguments.TokenType;
 import innovimax.mixthem.utils.StreamUtils;
 
 import java.util.List;
@@ -15,29 +15,29 @@ import java.util.stream.IntStream;
 */
 public class TokenRangeReader implements ITokenRange {
 
-	private final Token token;
+	private final TokenType tokenType;
 	private final List<ITokenInput> readers;
 	
 	/**
 	* Constructor
 	* @param inputs The list of inputs as InputResource
 	* @param selection The input index selection (maybe empty)
-	* @param token The input tokenization mode
+	* @param tokenType The input tokenization type
 	* @see innovimax.mixthem.io.InputResource
 	*/
-	public TokenRangeReader(final List<InputResource> inputs, final Set<Integer> selection, final Token token) {
-		this.token = token;
+	public TokenRangeReader(final List<InputResource> inputs, final Set<Integer> selection, final TokenType tokenType) {
+		this.tokenType = tokenType;
 		this.readers = IntStream.rangeClosed(1, inputs.size())
 			.filter(index -> selection.isEmpty() || selection.contains(Integer.valueOf(index)))
 			.mapToObj(index -> inputs.get(index-1))
 			.map(StreamUtils.apply(input -> {
-				switch(this.token) {
+				switch(this.tokenType) {
 					case BYTE: return new DefaultByteReader(input);
 					case CHAR: return new DefaultCharReader(input);
 					case LINE: return new DefaultLineReader(input);
 					case FILEBYTE: /*return new DefaultFileByteReader(input)*/ throw new RuntimeException("TODO");
 					case FILECHAR: /*return new DefaultFileCharReader(input)*/ throw new RuntimeException("TODO");
-					default: throw new UnsupportedOperationException("Token not expected: " + token.getName());
+					default: throw new UnsupportedOperationException("Token not expected: " + tokenType.getName());
 				}
 			}))
 			.collect(Collectors.toList());
@@ -49,13 +49,13 @@ public class TokenRangeReader implements ITokenRange {
 		//	see below...
 		return this.readers.stream()
 			.anyMatch(StreamUtils.test(reader -> {
-				switch(this.token) {
+				switch(this.tokenType) {
 					case BYTE: return ((IByteInput) reader).hasByte();
 					case CHAR: return ((ICharInput) reader).hasCharacter();
 					case LINE: return ((ILineInput) reader).hasLine();
 					case FILEBYTE: /*((IFileInput) reader).hasFile()*/ throw new RuntimeException("TODO");
 					case FILECHAR: /*((IFileInput) reader).hasFile()*/ throw new RuntimeException("TODO");
-					default: throw new UnsupportedOperationException("Token not expected: " + token.getName());
+					default: throw new UnsupportedOperationException("Token not expected: " + tokenType.getName());
 				}
 			}));
 	}
@@ -67,7 +67,7 @@ public class TokenRangeReader implements ITokenRange {
 		//	add hasMoreTokens, nextToken in ITokenInput
 		//	remove IByteInput, ICharInput, ILineInput
 		//	DefaultByteReader, DefaultCharReader, DefaultLineReader directly implements ITokenInput
-		switch(this.token) {
+		switch(this.tokenType) {
 			case BYTE: return readers.stream()
 					.map(StreamUtils.apply(reader -> ((IByteInput) reader).nextByte()))
 					.map(b -> InputToken.createByteToken(b))
@@ -84,7 +84,7 @@ public class TokenRangeReader implements ITokenRange {
 					.collect(Collectors.toList());
 			case FILEBYTE: /*((IFileInput) reader).hasFile()*/ throw new RuntimeException("TODO");
 			case FILECHAR: /*((IFileInput) reader).hasFile()*/ throw new RuntimeException("TODO");
-			default: throw new UnsupportedOperationException("Token not expected: " + token.getName());
+			default: throw new UnsupportedOperationException("Token not expected: " + tokenType.getName());
 		}
 	}
 	
