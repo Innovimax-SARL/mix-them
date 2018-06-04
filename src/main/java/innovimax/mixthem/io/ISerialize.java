9d02ef6db5013ee2014424ba930e2d918f204f5e
Innovimax team <kojimch@gmail.com>
2018-06-04 10:52:22 +0200

    Add serialization interface

---
+++ src/main/java/innovimax/mixthem/io/ISerialize.java
@@ -0,0 +1,22 @@
+package innovimax.mixthem.io;
+
+import java.io.IOException;
+
+/**
+* This interface provides for writing tokens in an output.
+* @author Innovimax
+* @version 1.0
+*/
+public interface ISerialize {
+	/**
+ 	* Writes a token.
+ 	* @param c The token to be written
+ 	* @throws IOException - If an I/O error occurs
+ 	*/	
+	void writeToken(IToken token) throws IOException;
+	/**
+	* Closes this output and releases any system resources associated with it.
+ 	* @throws IOException - If an I/O error occurs	
+	*/
+	void close() throws IOException;		
+}
