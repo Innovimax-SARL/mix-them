0e3e669cf904b03ace32e8dd9a687577e04de878
Innovimax team <kojimch@gmail.com>
2018-06-06 16:13:59 +0200

    Implement abstract token operation

--- src/main/java/innovimax/mixthem/operation/AbstractTokenOperation.java
+++ src/main/java/innovimax/mixthem/operation/AbstractTokenOperation.java
@@ -5,13 +5,19 @@ import innovimax.mixthem.arguments.ParamValue;
 import innovimax.mixthem.arguments.RuleParam;
 import innovimax.mixthem.arguments.TokenType;
 import innovimax.mixthem.io.InputResource;
+import innovimax.mixthem.io.ITokenRange;
+import innovimax.mixthem.io.ISerialize;
 import innovimax.mixthem.io.Token;
+import innovimax.mixthem.io.TokenRangeReader;
+import innovimax.mixthem.io.TokenSerializer;
+import innovimax.mixthem.utils.StreamUtils;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 /**
 * <p>Abstract class for all token operation.</p>
@@ -35,13 +41,35 @@ public abstract class AbstractTokenOperation extends AbstractOperation implement
 
 	@Override
 	public void processFiles(final List<InputResource> inputs, final OutputStream output) throws MixException, IOException {
-		//TODO
+		final ITokenRange reader = new TokenRangeReader(inputs, this.selection, this.tokenType);
+		final ISerialize writer = new TokenSerializer(output, this.tokenType);
+		final TokenResult result = new TokenResult(inputs.size());		
+		while (reader.hasMoreTokens()) {
+			// read next token range depends on last result indicators
+			final List<Token> tokenRange = reader.nextTokenRange(result.getReadingRange());
+			// set range preserved lines from last result
+			IntStream.range(0, tokenRange.size())
+				.filter(index -> !result.getReadingRange().get(index).booleanValue())
+				.forEach(index -> tokenRange.set(index, result.getRangeToken(index)));
+			result.reset();
+			if (mixable(tokenRange)) {
+				// process mixing
+				process(tokenRange, result);
+				// write mixing result if has one
+				if (result.hasResult()) {
+					result.getResult()
+						.forEach(StreamUtils.consume(token -> writer.writeToken(token)));
+				}			
+			}			
+		}
+		reader.close();
+		writer.close();
     	}
 	
 	@Override
 	public boolean mixable(final List<Token> tokenRange) {
-		//TODO
-		return false;
+		return IntStream.range(0, tokenRange.size())
+			.allMatch(index -> tokenRange.get(index) != null);
 	}
 
 }
