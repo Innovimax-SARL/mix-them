db82d2433dd5439448a3cf1137e7afe214df4cac
Innovimax team <kojimch@gmail.com>
2018-06-08 11:32:49 +0200

    Update line alternation op using tokens

--- src/main/java/innovimax/mixthem/operation/DefaultLineAlternation.java
+++ src/main/java/innovimax/mixthem/operation/DefaultLineAlternation.java
@@ -4,10 +4,12 @@ import innovimax.mixthem.MixException;
 import innovimax.mixthem.arguments.ParamValue;
 import innovimax.mixthem.arguments.RuleParam;
 import innovimax.mixthem.arguments.TokenType;
+import innovimax.mixthem.io.ITokenRange;
+import innovimax.mixthem.io.Token;
 
-import java.util.List;
+//import java.util.List;
 import java.util.Map;
-import java.util.Random;
+//import java.util.Random;
 import java.util.Set;
 
 /**
@@ -17,11 +19,11 @@ import java.util.Set;
 * @author Innovimax
 * @version 1.0
 */
-public class DefaultLineAlternation extends AbstractLineOperation {
+public class DefaultLineAlternation extends AbstractTokenAlternation /*AbstractLineOperation*/ {
 
-	private final AltMode mode;
+	/*private final AltMode mode;
 	private int channel;
-	private final Random random;
+	private final Random random;*/
 
 	/**
 	* @param mode The alternate mode to process
@@ -31,15 +33,17 @@ public class DefaultLineAlternation extends AbstractLineOperation {
 	* @see innovimax.mixthem.operation.ParamValue
 	*/
 	public DefaultLineAlternation(final AltMode mode, final Set<Integer> selection, final TokenType tokenType, final Map<RuleParam, ParamValue> params) {
-		super(selection, tokenType, params);
+		super(mode, selection, tokenType, params);
+		/*super(selection, tokenType, params);
 		this.mode = mode;
 		this.channel = 0;
-		this.random = new Random(params.getOrDefault(RuleParam.RANDOM_SEED, AltOperation.DEFAULT_RANDOM_SEED.getValue()).asInt());
+		this.random = new Random(params.getOrDefault(RuleParam.RANDOM_SEED, AltOperation.DEFAULT_RANDOM_SEED.getValue()).asInt());*/
 	}	
 
 	@Override
-	public void process(final List<String> lineRange, final LineResult result) throws MixException {
-		int channel = this.mode == AltMode.NORMAL ? this.channel : this.random.nextInt(lineRange.size());
+	/*public void process(final List<String> lineRange, final LineResult result) throws MixException {*/
+	public void process(final ITokenRange tokenRange, final ITokenResult result) throws MixException {
+		/*int channel = this.mode == AltMode.NORMAL ? this.channel : this.random.nextInt(lineRange.size());
 		String line = lineRange.get(channel);
 		//System.out.println("RANGE="+lineRange.toString());
 		//System.out.println("CHANNEL="+channel+" LINE="+line);
@@ -54,15 +58,31 @@ public class DefaultLineAlternation extends AbstractLineOperation {
 				this.channel = 0;
 			}		
 		}
-		result.setResult(line);
+		result.setResult(line);*/
+		int channel = this.mode == AltMode.NORMAL ? this.channel : this.random.nextInt(tokenRange.size());
+		String line = tokenRange.getToken(channel).asString();
+		//System.out.println("RANGE="+lineRange.toString());
+		//System.out.println("CHANNEL="+channel+" LINE="+line);
+		if (line == null) {					
+			channel = nextChannel(tokenRange, channel);
+			line = tokenRange.getToken(channel).asString();
+			//System.out.println("NEW_CHANNEL="+channel+" LINE="+line);
+		}		
+		if (this.mode == AltMode.NORMAL) {
+			this.channel = channel + 1;
+			if (this.channel == tokenRange.size()) {
+				this.channel = 0;
+			}		
+		}
+		result.setResult(Token.createLineToken(line));
 	}
 	
 	@Override
-	public boolean mixable(final List<String> lineRange) {	
+	public boolean mixable(final ITokenRange tokenRange) {
 		return true;	
 	}
 
-	private int nextChannel(final List<String> lineRange, final int curChannel) {
+	/*private int nextChannel(final List<String> lineRange, final int curChannel) {
 		int channel = curChannel+1;
 		while (channel != curChannel) {
 			if (channel < lineRange.size()) {
@@ -76,6 +96,6 @@ public class DefaultLineAlternation extends AbstractLineOperation {
 			}
 		}
 		return channel;
-	}
+	}*/
 	
 }
